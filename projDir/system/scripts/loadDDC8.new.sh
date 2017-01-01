8521b36ed90c8c097f63d64529491470db7c41af
hcr <hcr@rds.hcr.net>
2017-01-01 00:08:37 +0000

    Moving HCR_instrument into ~/git, modifying scripts accordingly

--- projDir/system/scripts/loadDDC8.new.sh
+++ projDir/system/scripts/loadDDC8.new.sh
@@ -2,6 +2,6 @@
 mcsFile=~hcr/workspace/sd3c/firmware/bitstream/sd3c-ddc8.mcs.new
 
 echo "Loading ${mcsFile##*/} to the Pentek"
-ReadyFlowDir=~hcr/HCR_instrument/src/ReadyFlow7142_428_WinDriver1150/linux/1.0/x86_64
+ReadyFlowDir=~hcr/git/HCR_instrument/src/ReadyFlow7142_428_WinDriver1150/linux/1.0/x86_64
 LD_LIBRARY_PATH=$ReadyFlowDir/lib
 $ReadyFlowDir/examples/fpgaload.out $mcsFile
