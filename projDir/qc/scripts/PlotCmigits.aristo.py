7c382193e87bb262359c16de929ac5d38fa812b1
mike-dixon <dixon@ucar.edu>
2017-03-29 00:28:00 +0000

    adding

--- projDir/qc/scripts/PlotCmigits.aristo.py
+++ projDir/qc/scripts/PlotCmigits.aristo.py
@@ -2,7 +2,7 @@
 
 #===========================================================================
 #
-# Produce plots for SPATIAL FOG data for ARISTO
+# Produce plots for CMIGITS data for ARISTO
 #
 #===========================================================================
 
@@ -31,11 +31,12 @@ def main():
     global figNum
     figNum = 0
 
-    appName = 'PlotSpatialFog.py'
+    appName = 'PlotCmigits.aristo.py'
     projDir = os.environ['PROJ_DIR']
     dataDir = os.environ['DATA_DIR']
     global dataFilePath
-    dataFilePath = os.path.join(dataDir, 'fog/SpatialFog_RF04.txt')
+    cmigitsFilePath = '/scr/eldora1/rsfdata/aristo-17/hcr/ascii/ac_georef/C-MIGITS_RF02.txt';
+    sdn500FilePath = '/scr/eldora1/rsfdata/aristo-17/hcr/ascii/ac_georef/ARISTO.rf02.20170224_193000.to.20170224_223500.txt';
 
     # parse the command line
 
@@ -45,10 +46,14 @@ def main():
                       dest='debug', default=False,
                       action="store_true",
                       help='Set debugging on')
-    parser.add_option('--file',
-                      dest='dataFilePath',
-                      default=dataFilePath,
-                      help='File path for comparison results')
+    parser.add_option('--cmigits_file',
+                      dest='cmigitsFilePath',
+                      default=cmigitsFilePath,
+                      help='File path for cmigits data')
+    parser.add_option('--sdn500_file',
+                      dest='sdn500FilePath',
+                      default=sdn500FilePath,
+                      help='File path for sdn500 data')
     parser.add_option('--widthMain',
                       dest='mainWidthMm',
                       default=300,
@@ -59,19 +64,19 @@ def main():
                       help='Height of main figure in mm')
     parser.add_option('--filtLen',
                       dest='filtLen',
-                      default=21,
+                      default=1,
                       help='Len of moving mean filter')
     parser.add_option('--decimation',
                       dest='decimation',
-                      default=20,
+                      default=1,
                       help='Decimation factor when reading in data')
     parser.add_option('--start',
                       dest='startTime',
-                      default='2016 10 10 16 00 00',
+                      default='2017 02 24 19 30 00',
                       help='Start time for XY plot')
     parser.add_option('--end',
                       dest='endTime',
-                      default='2016 10 10 17 45 00',
+                      default='2017 02 24 22 30 00',
                       help='End time for XY plot')
     
     (options, args) = parser.parse_args()
@@ -86,28 +91,43 @@ def main():
                                 int(hour), int(minute), int(sec))
 
     if (options.debug == True):
-        print >>sys.stderr, "  dataFilePath: ", options.dataFilePath
+        print >>sys.stderr, "  cmigitsFilePath: ", options.cmigitsFilePath
+        print >>sys.stderr, "  sdn500FilePath: ", options.sdn500FilePath
         print >>sys.stderr, "  startTime: ", startTime
         print >>sys.stderr, "  endTime: ", endTime
 
-    # read in column headers for bias results
+    # read in column headers for cmigits data
 
-    iret, compHdrs, compData = readColumnHeaders(options.dataFilePath)
+    iret, cmigitsHdrs, cmigitsData = readCmigitsColumnHeaders(options.cmigitsFilePath)
     if (iret != 0):
         sys.exit(-1)
 
-    # read in data for comp results
+    # read in data for cmigits results
 
-    compData, compTimes = readInputData(options.dataFilePath, compHdrs, compData)
+    cmigitsData, cmigitsTimes = readCmigitsInputData(options.cmigitsFilePath, cmigitsHdrs, cmigitsData)
 
     # load up the data arrays
 
-    loadDataArrays(compData, compTimes)
+    loadCmigitsDataArrays(cmigitsData, cmigitsTimes)
+
+    # read in column headers for sdn500 data
+
+    iret, sdn500Hdrs, sdn500Data = readSdn500ColumnHeaders(options.sdn500FilePath)
+    if (iret != 0):
+        sys.exit(-1)
+
+    # read in data for sdn500 results
+
+    sdn500Data, sdn500Times = readSdn500InputData(options.sdn500FilePath, sdn500Hdrs, sdn500Data)
+
+    # load up the data arrays
+
+    loadSdn500DataArrays(sdn500Data, sdn500Times)
 
     # render the plots
 
     doPlotPitchRollHeading()
-    #doPlotVelocity()
+    doPlotVelocity()
     doPlotPosition()
     #doPlotBodyAccel()
     #doPlotBodyRot()
@@ -121,10 +141,10 @@ def main():
     sys.exit(0)
     
 ########################################################################
-# Read columm headers for the data
+# Read columm headers for the cmigits data
 # this is in the first line
 
-def readColumnHeaders(filePath):
+def readCmigitsColumnHeaders(filePath):
 
     colHeaders = []
     colData = {}
@@ -151,9 +171,39 @@ def readColumnHeaders(filePath):
     return 0, colHeaders, colData
 
 ########################################################################
-# Read in the data
+# Read columm headers for the sdn500 data
+# this is in the first line
 
-def readInputData(filePath, colHeaders, colData):
+def readSdn500ColumnHeaders(filePath):
+
+    colHeaders = []
+    colData = {}
+
+    fp = open(filePath, 'r')
+    line = fp.readline()
+    fp.close()
+    
+    commentIndex = line.find("#")
+    if (commentIndex == 0):
+        # header
+        colHeaders = line.lstrip("# ").rstrip("\n").split()
+        if (options.debug == True):
+            print >>sys.stderr, "n colHeaders: ", len(colHeaders)
+            print >>sys.stderr, "colHeaders: ", colHeaders
+    else:
+        print >>sys.stderr, "ERROR - readColumnHeaders"
+        print >>sys.stderr, "  First line does not start with #"
+        return -1, colHeaders, colData
+    
+    for index, var in enumerate(colHeaders, start=0):
+        colData[var] = []
+        
+    return 0, colHeaders, colData
+
+########################################################################
+# Read in the cmigits data
+
+def readCmigitsInputData(filePath, colHeaders, colData):
 
     # open file
 
@@ -211,6 +261,68 @@ def readInputData(filePath, colHeaders, colData):
     return colData, obsTimes
 
 ########################################################################
+# Read in the sdn500 data
+
+def readSdn500InputData(filePath, colHeaders, colData):
+
+    # open file
+
+    fp = open(filePath, 'r')
+    lines = fp.readlines()
+    lineNum = 0
+
+    # read in a line at a time, set colData
+    for line in lines:
+        
+        commentIndex = line.find("#")
+        if (commentIndex >= 0):
+            continue
+
+        lineNum = lineNum + 1
+        if ((lineNum % int(options.decimation)) != 0):
+            continue
+            
+        # data
+        
+        data = line.strip().split()
+        if (len(data) != len(colHeaders)):
+            if (options.debug == True):
+                print >>sys.stderr, "skipping line: ", line
+                print >>sys.stderr, "  len(data): ", len(data)
+            continue;
+
+        for index, var in enumerate(colHeaders, start=0):
+            # print >>sys.stderr, "index, data[index]: ", index, ", ", data[index]
+            if (var == 'year' or var == 'month' or var == 'day' or \
+                var == 'hour' or var == 'min' or var == 'sec'):
+                colData[var].append(int(data[index]))
+            else:
+                colData[var].append(float(data[index]))
+
+    fp.close()
+
+    # load observation times array
+
+    year = colData['year']
+    month = colData['month']
+    day = colData['day']
+    hour = colData['hour']
+    min = colData['min']
+    sec = colData['sec']
+    # microsec = colData['microsecond']
+
+    obsTimes = []
+    for ii, var in enumerate(year, start=0):
+        #        print >>sys.stderr, "year, month, day, hour, min, sec: ", \
+        #            year[ii], ", ", month[ii], ", ", day[ii], ", ", \
+        #            hour[ii], ", ", min[ii], ", ", sec[ii]
+        thisTime = datetime.datetime(year[ii], month[ii], day[ii],
+                                     hour[ii], min[ii], sec[ii])
+        obsTimes.append(thisTime)
+
+    return colData, obsTimes
+
+########################################################################
 # Moving average filter
 
 def movingAverage(values, window):
@@ -225,30 +337,56 @@ def movingAverage(values, window):
 ########################################################################
 # Set up arrays for plotting
 
-def loadDataArrays(compData, compTimes):
+def loadCmigitsDataArrays(cmigitsData, cmigitsTimes):
 
     filtLen = int(options.filtLen)
     
     # set up arrays
 
-    global ctimes
+    global cTimes
 
-    ctimes = np.array(compTimes).astype(datetime.datetime)
+    cTimes = np.array(cmigitsTimes).astype(datetime.datetime)
 
-    global lat, lon, alt
-    lat = movingAverage(np.array(compData["latitude_deg"]).astype(np.double), filtLen)
-    lon = movingAverage(np.array(compData["longitude_deg"]).astype(np.double), filtLen)
-    alt = movingAverage(np.array(compData["altitude_m"]).astype(np.double), filtLen)
+    global cLat, cLon, cAlt
+    cLat = movingAverage(np.array(cmigitsData["latitude_deg"]).astype(np.double), filtLen)
+    cLon = movingAverage(np.array(cmigitsData["longitude_deg"]).astype(np.double), filtLen)
+    cAlt = movingAverage(np.array(cmigitsData["altitude_m"]).astype(np.double), filtLen)
 
-    global velNorth, velEast, velUp
-    velNorth = movingAverage(np.array(compData["velNorth_m_s-1"]).astype(np.double), filtLen)
-    velEast = movingAverage(np.array(compData["velEast_m_s-1"]).astype(np.double), filtLen)
-    velUp = movingAverage(np.array(compData["velUp_m_s-1"]).astype(np.double), filtLen)
+    global cVelNorth, cVelEast, cVelUp
+    cVelNorth = movingAverage(np.array(cmigitsData["velNorth_m_s-1"]).astype(np.double), filtLen)
+    cVelEast = movingAverage(np.array(cmigitsData["velEast_m_s-1"]).astype(np.double), filtLen)
+    cVelUp = movingAverage(np.array(cmigitsData["velUp_m_s-1"]).astype(np.double), filtLen)
 
-    global pitch, roll, heading
-    pitch = movingAverage(np.array(compData["pitch_deg"]).astype(np.double), filtLen)
-    roll = movingAverage(np.array(compData["roll_deg"]).astype(np.double), filtLen)
-    heading = movingAverage(np.array(compData["heading_deg"]).astype(np.double), filtLen)
+    global cPitch, cRoll, cHeading
+    cPitch = movingAverage(np.array(cmigitsData["pitch_deg"]).astype(np.double), filtLen)
+    cRoll = movingAverage(np.array(cmigitsData["roll_deg"]).astype(np.double), filtLen)
+    cHeading = movingAverage(np.array(cmigitsData["heading_deg"]).astype(np.double), filtLen)
+    
+########################################################################
+# Set up arrays for plotting
+
+def loadSdn500DataArrays(sdn500Data, sdn500Times):
+
+    filtLen = int(options.filtLen)
+    
+    # set up arrays
+
+    global sTimes
+
+    sTimes = np.array(sdn500Times).astype(datetime.datetime)
+
+    global sLat, sLon, sAlt
+    sLat = movingAverage(np.array(sdn500Data["lat"]).astype(np.double), filtLen)
+    sLon = movingAverage(np.array(sdn500Data["lon"]).astype(np.double), filtLen)
+    sAlt = movingAverage(np.array(sdn500Data["altKm"]).astype(np.double), filtLen) * 1000.0
+
+    global sVelUp
+    sVelUp = movingAverage(np.array(sdn500Data["vertVel"]).astype(np.double), filtLen)
+
+    global sPitch, sRoll, sHeading
+    sPitch = movingAverage(np.array(sdn500Data["pitch"]).astype(np.double), filtLen)
+    sRoll = movingAverage(np.array(sdn500Data["roll"]).astype(np.double), filtLen)
+    sHeading = movingAverage(np.array(sdn500Data["heading"]).astype(np.double), filtLen)
     
 ########################################################################
 # Plot pitch, roll and heading data
@@ -268,9 +406,14 @@ def doPlotPitchRollHeading():
     ax2 = fig.add_subplot(3,1,2,xmargin=0.0)
     ax3 = fig.add_subplot(3,1,3,xmargin=0.0)
     
-    ax1.plot(ctimes, pitch, label='Pitch(deg)', color='green', linewidth=1)
-    ax2.plot(ctimes, roll, label='Roll(deg)', color='red', linewidth=1)
-    ax3.plot(ctimes, heading, label='Heading(deg)', color='blue', linewidth=1)
+    ax1.plot(cTimes, cPitch, label='Cmigits Pitch(deg)', color='red', linewidth=1)
+    ax1.plot(sTimes, sPitch, label='Sdn500  Pitch(deg)', color='blue', linewidth=1)
+
+    ax2.plot(cTimes, cRoll, label='Cmigits Roll(deg)', color='red', linewidth=1)
+    ax2.plot(sTimes, sRoll, label='Sdn500  Roll(deg)', color='blue', linewidth=1)
+
+    ax3.plot(cTimes, cHeading, label='Cmigits Heading(deg)', color='red', linewidth=1)
+    ax3.plot(sTimes, sHeading, label='Sdn500  Heading(deg)', color='blue', linewidth=1)
 
     configTimeAxis(ax1, -9999, -9999, "Pitch(deg)", 'upper right')
     configTimeAxis(ax2, -9999, -9999, "Roll(deg)", 'upper right')
@@ -282,7 +425,7 @@ def doPlotPitchRollHeading():
     
     # title name
 
-    fig.suptitle("FOG PITCH/ROLL/HDG - " + str(startTime) + " to " + str(endTime))
+    fig.suptitle("PITCH/ROLL/HDG - " + str(startTime) + " to " + str(endTime))
 
     return
 
@@ -304,9 +447,12 @@ def doPlotVelocity():
     ax2 = fig.add_subplot(3,1,2,xmargin=0.0)
     ax3 = fig.add_subplot(3,1,3,xmargin=0.0)
     
-    ax1.plot(ctimes, velNorth, label='VelNorth(m/s)', color='green', linewidth=1)
-    ax2.plot(ctimes, velEast, label='VelEast(m/s)', color='red', linewidth=1)
-    ax3.plot(ctimes, velUp, label='VelUp(m/s)', color='blue', linewidth=1)
+    ax1.plot(cTimes, cVelNorth, label='Cmigits VelNorth(m/s)', color='red', linewidth=1)
+
+    ax2.plot(cTimes, cVelEast, label='Cmigits VelEast(m/s)', color='red', linewidth=1)
+
+    ax3.plot(cTimes, cVelUp, label='Cmigits VelUp(m/s)', color='red', linewidth=1)
+    ax3.plot(sTimes, sVelUp, label='Sdn500 VelUp(m/s)', color='blue', linewidth=1)
 
     configTimeAxis(ax1, -9999, -9999, "VelNorth", 'upper right')
     configTimeAxis(ax2, -9999, -9999, "VelEast", 'upper right')
@@ -318,7 +464,7 @@ def doPlotVelocity():
     
     # title name
 
-    fig.suptitle("FOG VELOCITIES - " + str(startTime) + " to " + str(endTime))
+    fig.suptitle("VELOCITIES - " + str(startTime) + " to " + str(endTime))
 
     return
 
@@ -340,14 +486,19 @@ def doPlotPosition():
     ax2 = fig.add_subplot(3,1,2,xmargin=0.0)
     ax3 = fig.add_subplot(3,1,3,xmargin=0.0)
     
-    ax1.plot(ctimes, lat, label='latitude', color='green', linewidth=1)
-    ax2.plot(ctimes, lon, label='longitude', color='red', linewidth=1)
-    ax3.plot(ctimes, alt, label='altitude(m)', color='blue', linewidth=1)
+    ax1.plot(cTimes, cLat, label='cmigits latitude', color='red', linewidth=1)
+    ax1.plot(sTimes, sLat, label='sdn500 latitude', color='blue', linewidth=1)
 
-    #configTimeAxis(ax1, -9999, -9999, "latitude", 'upper right')
-    #configTimeAxis(ax2, -9999, -9999, "longitude", 'upper right')
-    configTimeAxis(ax1, 39, 43, "latitude", 'upper right')
-    configTimeAxis(ax2, -106, -100, "longitude", 'upper right')
+    ax2.plot(cTimes, cLon, label='cmigits longitude', color='red', linewidth=1)
+    ax2.plot(sTimes, sLon, label='sdn500 longitude', color='blue', linewidth=1)
+
+    ax3.plot(cTimes, cAlt, label='cmigits altitude(m)', color='red', linewidth=1)
+    ax3.plot(sTimes, sAlt, label='sdn500 altitude(m)', color='blue', linewidth=1)
+    
+    configTimeAxis(ax1, -9999, -9999, "latitude", 'upper right')
+    configTimeAxis(ax2, -9999, -9999, "longitude", 'upper right')
+    #configTimeAxis(ax1, 39, 43, "latitude", 'upper right')
+    #configTimeAxis(ax2, -106, -100, "longitude", 'upper right')
     configTimeAxis(ax3, -9999, -9999, "altitude", 'upper right')
 
     fig.autofmt_xdate()
