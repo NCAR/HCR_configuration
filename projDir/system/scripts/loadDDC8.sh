a47c8b735ba0f9ad2b1d5a767ef1637f4087face
Mike Dixon <dixon@ucar.edu>
2018-10-31 16:32:37 +0000

    Change ReadyFlow handling

--- projDir/system/scripts/loadDDC8.sh
+++ projDir/system/scripts/loadDDC8.sh
@@ -2,6 +2,6 @@
 mcsFile=~hcr/workspace/sd3c/firmware/bitstream/sd3c-ddc8.mcs
 
 echo "Loading ${mcsFile##*/} to the Pentek"
-ReadyFlowDir=~hcr/git/HCR_instrument/src/ReadyFlow7142_428_WinDriver1150/linux/1.0/x86_64
-LD_LIBRARY_PATH=$ReadyFlowDir/lib
+export ReadyFlowDir=~hcr/git/HCR_instrument/src/ReadyFlow7142_428_WinDriver1150/linux/1.0/x86_64
+export LD_LIBRARY_PATH=$ReadyFlowDir/lib
 $ReadyFlowDir/examples/fpgaload.out $mcsFile
