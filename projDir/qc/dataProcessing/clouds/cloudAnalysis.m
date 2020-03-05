2b3597a10ac9429dc5912ac0af594c7f73139b96
ulrom <romatsch@ucar.edu>
2020-03-04 17:29:49 -0700

    Working on melting layer detection

--- projDir/qc/dataProcessing/clouds/cloudAnalysis.m
+++ projDir/qc/dataProcessing/clouds/cloudAnalysis.m
@@ -3,65 +3,54 @@
 clear all;
 close all;
 
-startTime=datetime(2018,1,23,3,30,0);
-endTime=datetime(2018,1,23,3,40,0);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
+% startTime=datetime(2018,2,24,0,0,0);
+% endTime=datetime(2018,2,24,23,0,0);
 
-% If 1, plots for individual calibration events will be made, if 0, only
-% total plots will be made
-makeSingleFigs=1;
-addNSCAL=0; % Noise source cal will be incorporated
-addSIG0model=1; % Plot also model data from sig0 model
+startTime=datetime(2019,8,7,0,0,0);
+endTime=datetime(2019,8,7,23,0,0);
 
-project='socrates'; %socrates, aristo, cset
-quality='qc1'; %field, qc1, or qc2
+plotFields=0;
+plotWholeFlight=1;
+
+project='otrec'; %socrates, aristo, cset
+quality='qc2'; %field, qc1, or qc2
 freqData='10hz'; % 10hz, 100hz, or 2hz
-whichModel='era5';
-addName=''; % Extra name part for output files. Default is ''.
 
-salinity=35; % Ocean salinity for sig0model in per mille (world wide default is 35) and sensitivity to that number is low
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-addpath('/h/eol/romatsch/gitPriv/process_HCR/oceanScans/functions/');
-addpath('/h/eol/romatsch/gitPriv/process_HCR/oceanScans/colormaps/');
-addpath('/h/eol/romatsch/gitPriv/process_HCR/NSCAL/functions/');
-addpath(genpath('/h/eol/romatsch/gitPriv/utils/'));
-
-outName=[project,'_stratConv'];
-
-outName=[outName,addName];
-
-directories.figdir=['/h/eol/romatsch/hcrCalib/stratConv/',outName,'/2hz/'];
-
-if ~exist(directories.figdir, 'dir')
-    mkdir(directories.figdir)
+% Expected bright band altitude. Determines plot zoom.
+if strcmp(project,'otrec')
+    expBBalt=5;
+    ylimits=[-0.2 15];
+elseif strcmp(project,'socrates')
+    expBBalt=2;
+    ylimits=[-0.2 9];
+elseif strcmp(project,'otrec')
+    expBBalt=5;
+    ylimits=[-0.2 9];
 end
 
-directories.dataDir=HCRdir(project,quality,freqData);
+addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
 
-if strcmp(whichModel,'era5')
-    directories.modeldir=['/scr/sci/romatsch/data/reanalysis/ecmwf/era5interp/',project,'/',freqData,'/'];
-elseif strcmp(whichModel,'ecmwf')
-    directories.modeldir=['/scr/sci/romatsch/data/reanalysis/ecmwf/forecastInterp/',project,'/',freqData,'/'];
+figdir=['/h/eol/romatsch/hcrCalib/clouds/',project,'/'];
+
+if ~exist(figdir, 'dir')
+    mkdir(figdir)
 end
+
+indir=HCRdir(project,quality,freqData);
 
 %% Load data
 
+disp('Loading data ...');
+
 data.DBZ=[];
-%data.VEL=[];
-%data.VEL_RAW=[];
-%data.VEL_CORR=[];
-%data.WIDTH=[];
-%data.WIDTH_CORR=[];
-%data.DBMVC=[];
-%data.SNR=[];
-%data.NCP=[];
-%data.LDR=[];
+data.LDR=[];
+data.TEMP=[];
+data.FLAG=[];
 
 dataVars=fieldnames(data);
 
 % Make list of files within the specified time frame
-fileList=makeFileList(directories.dataDir,startTime,endTime,'xxxxxx20YYMMDDxhhmmss',1);
+fileList=makeFileList(indir,startTime,endTime,'xxxxxx20YYMMDDxhhmmss',1);
 
 if length(fileList)==0
     disp('No data files found.');
@@ -80,121 +69,243 @@ end
 
 dataVars=dataVars(~cellfun('isempty',dataVars));
 
-%% Reflectivity profile
-
-refl=data.DBZ;
-
-% Remove upward pointing data
-upInd=find(data.elevation>0);
-%refl(:,upInd)=nan;
-
-% Remove bang
-refl(1:20,:)=nan;
-
-% Remove everything below surface+10 gates
-[surfLinInds surfRowInds rangeToSurf]=hcrSurfInds(data);
-surfIndsRM=surfRowInds-10;
-
-maskRM=zeros(size(data.range));
-
-for ii=1:size(maskRM,2)
-    if data.elevation(ii)<0 & ~isnan(surfIndsRM(ii))
-        maskRM(round(surfIndsRM(ii)):end,ii)=1;
-    end
-end
-
-refl(maskRM==1)=nan;
-data.DBZgood=refl;
-dataVars{1}='DBZgood';
-
-data.asl=HCRrange2asl(data.range,data.elevation,data.altitude);
-
-%% era5 data
-
-%model.asl=[];
-%model.p=[];
-model.rh=[];
-model.temp=[];
-%model.u=[];
-%model.v=[];
-%model.sst=[];
-%model.time=[];
-
-model=read_model(model,directories.modeldir,data.time(1),data.time(end));
-
 %% Find zero degree altitude
-tempVec=1:1:size(model.temp,1);
-tempMat=repmat(tempVec,size(model.temp,2),1)';
 
-tempMat(model.temp>0)=nan;
-tempMat(isnan(model.temp))=nan;
+disp('Searching 0 deg altitude ...');
+upInd=find(data.elevation>0);
+
+tempVec=1:1:size(data.TEMP,1);
+tempMat=repmat(tempVec,size(data.TEMP,2),1)';
+
+tempMat(data.TEMP>0)=nan;
+tempMat(isnan(data.TEMP))=nan;
 
 [maxs,rowInds] = nanmax(tempMat);
-colInds=1:1:size(model.temp,2);
+colInds=1:1:size(data.TEMP,2);
 
-linearInd = sub2ind(size(model.temp), rowInds, colInds);
+linearInd = sub2ind(size(data.TEMP), rowInds, colInds);
 
 [mins,rowIndsUp] = nanmin(tempMat);
-colIndsUp=1:1:size(model.temp,2);
+colIndsUp=1:1:size(data.TEMP,2);
 
-linearIndUp = sub2ind(size(model.temp), rowIndsUp, colIndsUp);
+linearIndUp = sub2ind(size(data.TEMP), rowIndsUp, colIndsUp);
 
 linearInd(upInd)=linearIndUp(upInd);
 rowInds(upInd)=rowIndsUp(upInd);
 
-checkTemps=model.temp(linearInd);
+checkTemps=data.TEMP(linearInd);
 outInds=zeros(size(checkTemps));
 outInds(checkTemps>0 | checkTemps<-2)=1;
 outInds(isnan(checkTemps))=1;
 linearInd(outInds==1)=[];
 zTempsNeg=data.asl(linearInd);
+[zSubR zSubC]=ind2sub(size(data.asl),linearInd);
 
-timeMat=repmat(data.time,size(model.temp,1),1);
+zTnegFull=nan(size(data.time));
+zTnegFull(zSubC)=zTempsNeg;
+
+timeMat=repmat(data.time,size(data.TEMP,1),1);
 
 %% Plot
 
 close all
-figs=plot_HCR(data,dataVars,[0 9]);
-figure(figs.fig1)
-hold on
-plot(timeMat(linearInd),zTempsNeg./1000,'k','linewidth',1.5);
 
-ax = gca;
-ax.SortMethod = 'childorder';
-
-formatOut = 'yyyymmdd_HHMM';
-set(gcf,'PaperPositionMode','auto')
-print([directories.figdir,datestr(startTime,formatOut),'_to_',datestr(endTime,formatOut),'_zeroDegree'],'-dpng','-r0');
-
+if plotFields
+    fig1=figure('DefaultAxesFontSize',11,'position',[100,100,1200,800]);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%% DBZ%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    ax1=subplot(2,1,1);
+    hold on;
+    
+    sub1=surf(data.time,data.asl./1000,data.DBZ,'edgecolor','none');
+    view(2);
+    sub1=colMapDBZ(sub1);
+    plot(timeMat(linearInd),zTempsNeg./1000,'c','linewidth',2);
+    ax = gca;
+    ax.SortMethod = 'childorder';
+    ylim(ylimits);
+    ylabel('Altitude (km)');
+    xlim([data.time(1),data.time(end)]);
+    title('Reflectivity and ERA5 freezing level')
+    grid on
+    
+    ax2=subplot(2,1,2);
+    hold on;
+    sub2=surf(data.time,data.asl./1000,data.LDR,'edgecolor','none');
+    view(2);
+    %plot(timeMat(linearInd),zTempsNeg./1000,'c','linewidth',2);
+    colorbar
+    ax = gca;
+    ax.SortMethod = 'childorder';
+    ylim(ylimits);
+    ylabel('Altitude (km)');
+    xlim([data.time(1),data.time(end)]);
+    title('LDR')
+    grid on
+    
+    formatOut = 'yyyymmdd_HHMM';
+    set(gcf,'PaperPositionMode','auto')
+    print([figdir,'refl_',datestr(startTime,formatOut),'_to_',datestr(endTime,formatOut),'_zeroDegree'],'-dpng','-r0');
+end
 %% Find bright band
-bbAlt=nan(size(data.time));
+
+disp('Searching melting layer ...');
+maxLevel=nan(size(data.time));
+
+data.LDR(data.FLAG>1)=nan;
+BB=data.LDR;
+BB(BB<-16 | BB>-7)=nan;
+
+BB=smoothdata(BB,1,'movmedian',5);
+
+% Find altitude of bright band
 for ii=1:length(data.time)
-    if ~outInds(ii)
-        vertCol=data.DBZgood(:,ii);
+    vertCol=BB(:,ii);
+    if data.elevation(ii)<90 % Down pointing
         vertCol(rowInds(ii)+30:end)=nan;
-        vertCol(1:rowInds(ii)-30)=nan;
-        
-        % Check if all nan
-        if min(isnan(vertCol))==1
-            continue
-        end
-        % Check if max refl value is at beginning or end
+        vertCol(1:max([rowInds(ii)-10,1]))=nan;
+    else % Up pointing
+        vertCol(rowInds(ii)+10:end)=nan;
+        vertCol(1:max([rowInds(ii)-30,1]))=nan;
+    end
+    
+    % Check if all nan
+    if min(isnan(vertCol))==0
         vertColData=vertCol(~isnan(vertCol));
-        %         if data.time(ii)>datetime(2018,2,7,21,10,0)
-        %             figure
-        %             plot(vertCol);
-        %             test1=1;
-        %         end
-        if find(vertColData==max(vertColData))==1 | find(vertColData==max(vertColData))==length(vertColData)
-            continue
-        else
-            bbAlt(ii)=find(vertCol==nanmax(vertCol));
+        maxLevel(ii)=min(find(vertCol==nanmax(vertCol)));
+    end
+end
+
+colIndsBB=1:1:size(data.TEMP,2);
+linearIndBB = sub2ind(size(data.TEMP), maxLevel, colIndsBB);
+linearIndBB(isnan(maxLevel))=[];
+
+BBaltRaw=data.asl(linearIndBB);
+BBalt=movmedian(BBaltRaw,300,'omitnan');
+
+BBaltFull=nan(size(data.time));
+BBaltFull(find(~isnan(maxLevel)))=BBalt;
+
+BBloc=abs(BBaltRaw-BBalt);
+
+BBlocFull=nan(size(data.time));
+BBlocFull(find(~isnan(maxLevel)))=BBloc;
+
+BB(:,isnan(BBlocFull))=nan;
+BB(:,BBlocFull>200)=nan;
+
+%% Remove data that is too far off bright band altitude
+
+BBvalMax=BB(linearIndBB);
+BBvalFull=nan(size(data.time));
+BBvalFull(find(~isnan(maxLevel)))=BBvalMax;
+
+for ii=1:length(data.time)
+    BBcol=BB(:,ii);
+    dataInd=find(~isnan(BBcol));
+    if length(dataInd)>0
+        diffs=abs(data.asl(dataInd,ii)-BBaltFull(ii));
+        tooFar=find(diffs>300);
+        BB(dataInd(tooFar),ii)=nan;
+        % remove data in low gradient areas
+        BBcol=BB(:,ii);
+        dataInd=find(~isnan(BBcol));
+        if length(dataInd)>0
+            minVal=min(BBcol(dataInd));
+            %if minVal>-14
+            BB((BBvalFull(ii)-BBcol)>(BBvalFull(ii)-minVal)/1.2,ii)=nan;
+            %end
         end
     end
 end
 
-colIndsBB=1:1:size(model.temp,2);
-linearIndBB = sub2ind(size(model.temp), bbAlt, colIndsBB);
-linearIndBB(isnan(bbAlt))=[];
+% Remove contiguous areas that are too small
+specCut=100;
 
-plot(timeMat(linearIndBB),data.asl(linearIndBB)./1000,'b','linewidth',1.5);
+bbTemp=BB;
+
+maskTemp=zeros(size(bbTemp));
+maskTemp(~isnan(bbTemp))=1;
+
+CC = bwconncomp(maskTemp,4);
+
+for ii=1:CC.NumObjects
+    area=CC.PixelIdxList{ii};
+    if length(area)<=specCut
+        BB(area)=nan;
+    end
+end
+
+%% Plot
+if plotFields
+    fig2=figure('DefaultAxesFontSize',11,'position',[100,1300,1200,800]);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%% DBZ%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    ax1=subplot(2,1,1);
+    hold on;
+    
+    ax1=subplot(2,1,1);
+    hold on;
+    sub1=surf(data.time,data.asl./1000,data.LDR,'edgecolor','none');
+    view(2);
+    scatter(timeMat(linearIndBB),BBalt./1000,10,'m','filled');
+    scatter(timeMat(linearIndBB),data.asl(linearIndBB)./1000,10,'r','filled');
+    colorbar
+    ax = gca;
+    ax.SortMethod = 'childorder';
+    ylim([expBBalt-3 expBBalt+3]);
+    ylabel('Altitude (km)');
+    xlim([data.time(1),data.time(end)]);
+    title('LDR')
+    grid on
+    
+    ax2=subplot(2,1,2);
+    hold on;
+    sub2=surf(data.time,data.asl./1000,BB,'edgecolor','none');
+    view(2);
+    colorbar
+    ylim([expBBalt-3 expBBalt+3]);
+    ylabel('Altitude (km)');
+    xlim([data.time(1),data.time(end)]);
+    title('Bright band')
+    grid on
+    
+    formatOut = 'yyyymmdd_HHMM';
+    set(gcf,'PaperPositionMode','auto')
+    print([figdir,'bb_',datestr(startTime,formatOut),'_to_',datestr(endTime,formatOut),'_zeroDegree'],'-dpng','-r0');
+end
+
+%% DBZ and line plot
+if plotWholeFlight
+    
+    %% Re-sample altitude
+    BBdata=sum(BB,1,'omitnan');
+    BBnan=find(BBdata==0);
+    
+    BBaltFull(BBnan)=nan;
+    
+    newInds=1:100:length(data.time);
+    newDBZ=data.DBZ(:,newInds);
+    newASL=data.asl(:,newInds);
+    newTime=data.time(newInds);
+    
+    fig3=figure('DefaultAxesFontSize',11,'position',[100,1300,2500,600]);
+    
+    hold on;
+    sub1=surf(newTime,newASL./1000,newDBZ,'edgecolor','none');
+    view(2);
+    sub1=colMapDBZ(sub1);
+    scatter(data.time,BBaltFull./1000,10,'c','filled');
+    plot(data.time,zTnegFull./1000,'-k','linewidth',1.5);
+    ax = gca;
+    ax.SortMethod = 'childorder';
+    ylim(ylimits);
+    ylabel('Altitude (km)');
+    xlim([data.time(1),data.time(end)]);
+    title('Reflectivity and ERA5 freezing level')
+    grid on
+    
+    formatOut = 'yyyymmdd_HHMM';
+    set(gcf,'PaperPositionMode','auto')
+    print([figdir,'wholeFlight_',datestr(data.time(1),formatOut),'_to_',datestr(data.time(end),formatOut)],'-dpng','-r0');
+end
