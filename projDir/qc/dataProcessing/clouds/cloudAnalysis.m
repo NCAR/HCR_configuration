e2af236d46acff8635e16d7529b682da75c0beb0
ulrom <romatsch@ucar.edu>
2020-03-17 18:04:26 -0600

    Working on melting layer detection

--- projDir/qc/dataProcessing/clouds/cloudAnalysis.m
+++ projDir/qc/dataProcessing/clouds/cloudAnalysis.m
@@ -3,14 +3,14 @@
 clear all;
 close all;
 
-startTime=datetime(2018,2,5,0,10,0);
-endTime=datetime(2018,2,5,0,35,0);
+startTime=datetime(2018,2,4,12,10,0);
+endTime=datetime(2018,2,5,12,35,0);
 
 % startTime=datetime(2019,8,7,0,0,0);
 % endTime=datetime(2019,8,7,23,0,0);
 
-plotFields=1;
-plotWholeFlight=0;
+plotFields=0;
+plotWholeFlight=1;
 
 project='socrates'; %socrates, aristo, cset
 quality='qc2'; %field, qc1, or qc2
@@ -194,9 +194,13 @@ BBaltAll={};
 timeIall={};
 BBaltInterpAll={};
 BBaltZeroAll={};
+timeIZeroAll={};
+
+transLength=6000;
 
 for kk=1:size(layerAlts,1)
     timeInds=find(~isnan(layerAlts(kk,:)));
+    layerAltsTemp=layerAlts(kk,timeInds);
     
     rowInds=layerInds(kk,timeInds);
     maxLevel=nan(size(rowInds));
@@ -216,104 +220,142 @@ for kk=1:size(layerAlts,1)
         end
     end
     
-    if min(isnan(maxLevel))~=0
-        continue
-    end
-    
-    BBlayer=BB(:,timeInds);
-    ASLlayer=data.asl(:,timeInds);
-    
-    colIndsBB=1:1:size(BBlayer,2);
-    linearIndBB = sub2ind(size(BBlayer), maxLevel, colIndsBB);
-    linearIndBB(isnan(maxLevel))=[];
-    
-    % Raw altitude of melting layer
-    BBaltRawIn=ASLlayer(linearIndBB);
-    BBaltRaw=nan(size(rowInds));
-    BBaltRaw(find(~isnan(maxLevel)))=BBaltRawIn;
-    % Mean altitude of meltig layer
-    BBalt=movmedian(BBaltRaw,300,'omitnan');
-    BBalt(isnan(BBaltRaw))=nan;
-    % Standard deviation
-    BBaltS=movstd(BBaltRaw,300,'omitnan');
-    BBaltS(isnan(BBaltRaw))=nan;
-    % Remove data with too much std
-    BBaltRaw(BBaltS>100)=nan;
-    % Distance between raw and mean altitude
-    BBloc=abs(BBaltRaw-BBalt);
-    
-    % Remove data that is too short
-    BBmask=zeros(size(BBaltRaw));
-    BBmask(~isnan(BBaltRaw))=1;
-    
-    indMask=find(abs(diff(BBmask))==1);
-    diffMask=diff(indMask);
-    
-    diffOnes=find(diffMask==1);
-    indsOnes=indMask(diffOnes);
-    
-    for ii=1:length(indsOnes)
-        if BBmask(indsOnes(ii)+1)==0
-            BBmask(indsOnes(ii)+1)=1;
+    if min(isnan(maxLevel))==0
+        
+        BBlayer=BB(:,timeInds);
+        ASLlayer=data.asl(:,timeInds);
+        
+        colIndsBB=1:1:size(BBlayer,2);
+        linearIndBB = sub2ind(size(BBlayer), maxLevel, colIndsBB);
+        linearIndBB(isnan(maxLevel))=[];
+        
+        % Raw altitude of melting layer
+        BBaltRawIn=ASLlayer(linearIndBB);
+        BBaltRaw=nan(size(rowInds));
+        BBaltRaw(find(~isnan(maxLevel)))=BBaltRawIn;
+        % Mean altitude of meltig layer
+        BBalt=movmedian(BBaltRaw,300,'omitnan');
+        BBalt(isnan(BBaltRaw))=nan;
+        % Standard deviation
+        BBaltS=movstd(BBaltRaw,300,'omitnan');
+        BBaltS(isnan(BBaltRaw))=nan;
+        % Remove data with too much std
+        BBaltRaw(BBaltS>100)=nan;
+        % Distance between raw and mean altitude
+        BBloc=abs(BBaltRaw-BBalt);
+        
+        % Remove data that is too short
+        BBmask=zeros(size(BBaltRaw));
+        BBmask(~isnan(BBaltRaw))=1;
+        
+        indMask=find(abs(diff(BBmask))==1);
+        diffMask=diff(indMask);
+        
+        diffOnes=find(diffMask==1);
+        indsOnes=indMask(diffOnes);
+        
+        for ii=1:length(indsOnes)
+            if BBmask(indsOnes(ii)+1)==0
+                BBmask(indsOnes(ii)+1)=1;
+            end
         end
-    end
-    
-    % Keep data that has only a few nan
-    
-    zeroCut=100;
-    CC = bwconncomp(BBmask);
-    
-    for ii=1:CC.NumObjects
-        area=CC.PixelIdxList{ii};
-        if length(area)<=zeroCut
-            BBaltRaw(area)=nan;
+        
+        % Keep data that has only a few nan
+        
+        zeroCut=100;
+        CC = bwconncomp(BBmask);
+        
+        for ii=1:CC.NumObjects
+            area=CC.PixelIdxList{ii};
+            if length(area)<=zeroCut
+                BBaltRaw(area)=nan;
+            end
         end
-    end
-    
-    % Remove data where distance is more than 200 m
-    BBaltRaw(BBloc>50)=nan;
-    
-    % Avoid jumps between gates
-    BBaltRaw=movmedian(BBaltRaw,20,'omitnan');
-    
-    % Interpolate between good values
-    BBaltInterp=nan(size(BBaltRaw));
-    BBaltZero=nan(size(BBaltRaw));
-    
-    % Mask
-    maskBBalt=zeros(size(BBaltRaw));
-    maskBBalt(isnan(BBaltRaw))=1;
-    diffBBalt=diff(maskBBalt);
-    
-    endInds=find(diffBBalt==-1);
-    startInds=find(diffBBalt==1);
-    
-    startInds=startInds+1;
-    
-    if endInds(1)<startInds(1)
-        startInds=[1 startInds];
-    end    
-    if length(endInds)~=length(startInds)
-        endInds=[endInds length(maskBBalt)];
-    end
-    
-    % Go through each nan stretch
-    for ll=1:length(startInds)
-        nanLength=endInds(jj)-startInds(jj);
-        % Short stretches
-        if nanLength<6000
+        
+        % Remove data where distance is more than 200 m
+        BBaltRaw(BBloc>50)=nan;
+        
+        % Avoid jumps between gates
+        BBaltRaw=movmedian(BBaltRaw,20,'omitnan');
+        
+        % Interpolate between good values
+        BBaltInterp=nan(size(BBaltRaw));
+        BBaltZero=nan(size(BBaltRaw));
+        
+        if min(isnan(BBaltRaw))==0
+            % Mask
+            maskBBalt=zeros(size(BBaltRaw));
+            maskBBalt(isnan(BBaltRaw))=1;
+            diffBBalt=diff(maskBBalt);
             
+            newIndsMask=1:length(BBaltRaw);
+            
+            endInds=find(diffBBalt==-1);
+            startInds=find(diffBBalt==1);
+            
+            startInds=startInds+1;
+            
+            if endInds(1)<startInds(1)
+                startInds=[1 startInds];
+            end
+            if length(endInds)~=length(startInds)
+                endInds=[endInds length(maskBBalt)];
+            end
+            
+            % Go through each nan stretch
+            for ll=1:length(startInds)
+                nanLength=endInds(ll)-startInds(ll);
+                % Short stretches
+                if nanLength<transLength*2 & startInds(ll)~=1 & endInds(ll)~=length(maskBBalt)
+                    BBaltInterp(startInds(ll):endInds(ll))=interp1([startInds(ll)-1,endInds(ll)+1],...
+                        [BBaltRaw(startInds(ll)-1),BBaltRaw(endInds(ll)+1)],startInds(ll):endInds(ll));
+                else                   
+                    % Tail
+                    if startInds(ll~=1)
+                        startTail=startInds(ll);
+                        endTailIn=min([startInds(ll)+transLength,length(maskBBalt)]);
+                        modT=layerAltsTemp(startTail:endTailIn);
+                        [~,modTind]=min(abs(BBaltRaw(startTail-1)-modT));
+                        indsTemp=newIndsMask(startTail:endTailIn);
+                        endTail=indsTemp(modTind);
+                        
+                        modTtail=layerAltsTemp(endTail);
+                        BBaltZero(startTail:endTail)=interp1([startTail-1,endTail+1],...
+                            [BBaltRaw(startTail-1),modTtail],startTail:endTail);
+                    end
+                    % Head
+                    if endInds(ll)~=length(maskBBalt)
+                        startHeadIn=max([endInds(ll)-transLength,1]);
+                        endHead=endInds(ll);
+                        modT=layerAltsTemp(startHeadIn:endHead);
+                        [~,modTind]=min(abs(BBaltRaw(endHead+1)-modT));
+                        indsTemp=newIndsMask(startHeadIn:endHead);
+                        startHead=indsTemp(modTind);
+                        
+                        modThead=layerAltsTemp(startHead);
+                        BBaltZero(startHead:endHead)=interp1([startHead-1,endHead+1],...
+                            [modThead,BBaltRaw(endHead+1)],startHead:endHead);
+                    end
+                end
+            end
+            
+            BBaltZero(isnan(BBaltZero))=layerAltsTemp(isnan(BBaltZero));
+            BBaltZero(~isnan(BBaltRaw))=nan;
+            BBaltZero(~isnan(BBaltInterp))=nan;
         else
-            
+            BBaltZero=layerAlts(kk,timeInds);
         end
+        
+        if min(min(isnan(BBlayer)))==0
+            BBaltAll{end+1}=BBaltRaw;
+            timeIall{end+1}=timeInds;
+            BBaltInterpAll{end+1}=BBaltInterp;
+        end
+    else
+        BBaltZero=layerAlts(kk,timeInds);
     end
-    
-    if min(min(isnan(BBlayer)))==0
-        BBaltAll{end+1}=BBaltRaw;
-        timeIall{end+1}=timeInds;
-        BBaltInterpAll={};
-        BBaltZeroAll={};
-    end
+    BBaltZeroAll{end+1}=BBaltZero;
+    timeIZeroAll{end+1}=timeInds;
 end
 
 %% Put things back together
@@ -333,20 +375,50 @@ end
 
 % BB altitude
 for ii=1:size(BBaltAll,2)
-   BB1=BBaltAll{ii}; 
-   timeI1=timeIall{ii};
-   
-   timeI1(isnan(BB1))=[];
-   BB1(isnan(BB1))=[];
-   
-   for jj=1:length(BB1)
-       [min1 ind1]=(min(abs(data.asl(:,timeI1(jj))-BB1(jj))));
-       BBfinished(ind1,timeI1(jj))=1;
-   end
+    BB1=BBaltAll{ii};
+    timeI1=timeIall{ii};
+    
+    timeI1(isnan(BB1))=[];
+    BB1(isnan(BB1))=[];
+    
+    for jj=1:length(BB1)
+        [min1 ind1]=(min(abs(data.asl(:,timeI1(jj))-BB1(jj))));
+        BBfinished(ind1,timeI1(jj))=1;
+    end
+end
+
+% BB altitude interpolated
+for ii=1:size(BBaltInterpAll,2)
+    BB1=BBaltInterpAll{ii};
+    timeI1=timeIall{ii};
+    
+    timeI1(isnan(BB1))=[];
+    BB1(isnan(BB1))=[];
+    
+    for jj=1:length(BB1)
+        [min1 ind1]=(min(abs(data.asl(:,timeI1(jj))-BB1(jj))));
+        BBfinished(ind1,timeI1(jj))=2;
+    end
+end
+
+% BB altitude zero
+for ii=1:size(BBaltZeroAll,2)
+    BB1=BBaltZeroAll{ii};
+    timeI1=timeIZeroAll{ii};
+    
+    timeI1(isnan(BB1))=[];
+    BB1(isnan(BB1))=[];
+    
+    for jj=1:length(BB1)
+        [min1 ind1]=(min(abs(data.asl(:,timeI1(jj))-BB1(jj))));
+        BBfinished(ind1,timeI1(jj))=3;
+    end
 end
 
 zeroInds=find(BBfinished==0);
 oneInds=find(BBfinished==1);
+twoInds=find(BBfinished==2);
+threeInds=find(BBfinished==3);
 %% Plot
 
 close all
@@ -376,6 +448,8 @@ if plotFields
     view(2);
     sub2=colMapDBZ(sub2);
     scatter(timeMat(oneInds),data.asl(oneInds)./1000,10,'c','filled');
+    scatter(timeMat(twoInds),data.asl(twoInds)./1000,10,'b','filled');
+    scatter(timeMat(threeInds),data.asl(threeInds)./1000,10,'g','filled');
     ax = gca;
     ax.SortMethod = 'childorder';
     ylim(ylimits);
@@ -415,6 +489,8 @@ if plotFields
         caxis([-25 5]);
         colorbar
         scatter(timeMat(oneInds),data.asl(oneInds)./1000,10,'r','filled');
+        scatter(timeMat(twoInds),data.asl(twoInds)./1000,10,'m','filled');
+        scatter(timeMat(threeInds),data.asl(threeInds)./1000,10,'g','filled');
         ax = gca;
         ax.SortMethod = 'childorder';
         ylim(ylimits);
@@ -448,7 +524,22 @@ if plotWholeFlight
     sub1=surf(newTime,newASL./1000,newDBZ,'edgecolor','none');
     view(2);
     sub1=colMapDBZ(sub1);
+    colIndsAll=1:length(data.time);
+    for ii=1:size(layerAlts,1)
+        rowInds=layerInds(ii,:);
+        colInds=colIndsAll;
+        colInds(isnan(rowInds))=[];
+        rowInds(isnan(rowInds))=[];
+        
+        linPlot=sub2ind(size(data.asl),rowInds,colInds);
+        
+        rowAlts=layerAlts(ii,:);
+        rowAlts(isnan(rowAlts))=[];
+        scatter(timeMat(linPlot),rowAlts./1000,10,'filled');
+    end
     scatter(timeMat(oneInds),data.asl(oneInds)./1000,10,'c','filled');
+    scatter(timeMat(twoInds),data.asl(twoInds)./1000,10,'b','filled');
+    scatter(timeMat(threeInds),data.asl(threeInds)./1000,10,'g','filled');
     ax = gca;
     ax.SortMethod = 'childorder';
     ylim(ylimits);
@@ -456,23 +547,7 @@ if plotWholeFlight
     xlim([data.time(1),data.time(end)]);
     title('Reflectivity and ERA5 freezing level')
     grid on
-    
-    colIndsAll=1:length(data.time);
-    for ii=1:size(layerAlts,1)
-       rowInds=layerInds(ii,:);
-       colInds=colIndsAll;
-       colInds(isnan(rowInds))=[];
-       rowInds(isnan(rowInds))=[];
-              
-       linPlot=sub2ind(size(data.asl),rowInds,colInds);
-       
-       rowAlts=layerAlts(ii,:);
-       rowAlts(isnan(rowAlts))=[];
-       scatter(timeMat(linPlot),rowAlts./1000,10,'filled'); 
-    end
-    
-    %scatter(timeMat(zeroInds),data.asl(zeroInds)./1000,10,'b','filled');
-    
+        
     subplot(2,1,2)
     hold on;
     sub2=surf(newTime,newASL./1000,newLDR,'edgecolor','none');
