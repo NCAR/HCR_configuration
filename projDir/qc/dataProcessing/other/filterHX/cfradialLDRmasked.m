ace042035e06cd5d22a6d6a9a045670d6e313527
mike-dixon <dixon@ucar.edu>
2021-08-04 17:53:34 -0600

    Adding LDR correction

---
+++ projDir/qc/dataProcessing/other/filterHX/cfradialLDRmasked.m
@@ -0,0 +1,136 @@
+% Created masked LDR field
+clear all;
+close all;
+
+addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
+
+project='spicule'; % socrates, cset, aristo, otrec
+quality='qc1'; % field, qc1, qc2
+qcVersion='v1.0';
+freqData='10hz';
+whichModel='narr';
+
+formatOut = 'yyyymmdd';
+
+infile=['~/git/HCR_configuration/projDir/qc/dataProcessing/scriptsFiles/flights_',project,'_data.txt'];
+
+caseList = table2array(readtable(infile));
+
+indir=HCRdir(project,quality,qcVersion,freqData);
+
+%% Run processing
+
+% Go through flights
+for ii=1:size(caseList,1)
+    
+    disp(['Flight ',num2str(ii)]);
+    
+    startTime=datetime(caseList(ii,1:6));
+    endTime=datetime(caseList(ii,7:12));
+    
+    fileList=makeFileList(indir,startTime,endTime,'xxxxxx20YYMMDDxhhmmss',1);
+    
+    if ~isempty(fileList)
+        %% Loop through HCR data files
+        for jj=1:length(fileList)
+            
+            testField=[];;
+            
+            infile=fileList{jj};
+            disp(infile);
+            
+            try
+                testField=ncread(infile,'LDR_MASKED');
+            end
+            if ~isempty(testField)
+                warning('Field already exists. Skipping file.')
+                continue
+            end
+            
+            
+            
+            data.DBMHX=ncread(infile,'DBMHX');
+            data.LDR=ncread(infile,'LDR');
+            data.FLAG=ncread(infile,'FLAG');
+            
+            data.LDR(data.FLAG>1)=nan;
+            
+            %% Find indices where correction needs to be applied
+            medianHXend=median(data.DBMHX(760:770,:));
+            
+            corrInd=medianHXend>-96;
+            
+            if ~isempty(corrInd)
+                dbm=data.DBMHX(:,corrInd==1);
+                ldr=data.LDR(:,corrInd==1);
+                
+                %% Find filter indices
+                med=medianHXend(corrInd==1);
+                
+                %dbmMask=dbm>-94;
+                dbmMask=dbm>med+0.5;
+                
+                dbmMaskOut=zeros(size(dbmMask));
+                
+                for ii=1:size(dbmMask,2)
+                    rayIn=dbmMask(:,ii);
+                    dbmMaskOut(:,ii)=bwareaopen(rayIn,10);
+                end
+                
+                maskUse=dbmMaskOut==0 & dbmMask==1;
+                
+                filterLDR=ldr;
+                filterLDR(maskUse==1)=nan;
+                
+                LDR_masked=data.LDR;
+                LDR_masked(:,corrInd==1)=filterLDR;
+                
+            else
+                LDR_masked=data.LDR;
+            end
+            
+            
+            %
+            %             % Create masked VEL field
+            %             vel=ncread(infile,'VEL')';
+            %             maskFlag=ncread(infile,'FLAG')';
+            %             maskAnt=ncread(infile,'ANTFLAG')';
+            %
+            %             velMasked=vel;
+            %             velMasked(maskAnt>2,:)=nan;
+            %             velMasked(maskFlag>1)=nan;
+            %             velMasked=velMasked';
+            
+            % Write output
+            fillVal=-9999;
+            
+            % Open file
+            ncid = netcdf.open(infile,'WRITE');
+            netcdf.setFill(ncid,'FILL');
+            
+            % Get dimensions
+            dimtime = netcdf.inqDimID(ncid,'time');
+            dimrange = netcdf.inqDimID(ncid,'range');
+            
+            % Define variables
+            netcdf.reDef(ncid);
+            varidLDRm = netcdf.defVar(ncid,'LDR_MASKED','NC_FLOAT',[dimrange dimtime]);
+            netcdf.defVarFill(ncid,varidLDRm,false,fillVal);
+            netcdf.endDef(ncid);
+            
+            % Write variables
+            netcdf.putVar(ncid,varidLDRm,LDR_masked);
+            
+            netcdf.close(ncid);
+            
+            % Write attributes
+            ncwriteatt(infile,'LDR_MASKED','long_name','linear_depolarization_ratio');
+            ncwriteatt(infile,'LDR_MASKED','standard_name','log_linear_depolarization_ratio');
+            ncwriteatt(infile,'LDR_MASKED','units','dB');
+            ncwriteatt(infile,'LDR_MASKED','comment','This field is created by masking out invalid and non-cloud data.');
+            ncwriteatt(infile,'LDR_MASKED','grid_mapping','grid_mapping');
+            ncwriteatt(infile,'LDR_MASKED','coordinates','time range');
+            
+        end
+    end
+end
--- projDir/qc/dataProcessing/other/filterHX/filterHX.m
+++ projDir/qc/dataProcessing/other/filterHX/filterHX.m
@@ -3,8 +3,8 @@
 clear all;
 close all;
 
-startTime=datetime(2021,6,11,18,45,0);
-endTime=datetime(2021,6,11,19,5,0);
+startTime=datetime(2021,6,21,3,50,0);
+endTime=datetime(2021,6,21,3,56,0);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -15,92 +15,133 @@ qcVersion='v1.0';
 
 addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
 
-%figdir=['/scr/sleet2/rsfdata/projects/spicule/hcr/qc1/cfradial/v1.0/flagPlots/'];
+figdir=['/scr/sleet2/rsfdata/projects/spicule/hcr/qc1/cfradial/v1.0/hxPlots/'];
 
 % if ~exist(figdir, 'dir')
 %     mkdir(figdir)
 % end
 
+casefile=['~/git/HCR_configuration/projDir/qc/dataProcessing/other/filterHX/fixLDRtimes.txt'];
+
 dataDir=HCRdir(project,quality,qcVersion,freqData);
 %dataDir='/scr/sleet2/rsfdata/projects/spicule/hcr/qc1/cfradial/moments/10hz/';
 
-%% Load data
+% Loop through cases
 
-data.DBMHX=[];
-data.LDR=[];
-data.FLAG=[];
+caseList=readtable(casefile);
+caseStart=datetime(caseList.Var1,caseList.Var2,caseList.Var3, ...
+    caseList.Var4,caseList.Var5,0);
+caseEnd=datetime(caseList.Var6,caseList.Var7,caseList.Var8, ...
+    caseList.Var9,caseList.Var10,0);
 
-dataVars=fieldnames(data);
-
-% Make list of files within the specified time frame
-fileList=makeFileList(dataDir,startTime,endTime,'xxxxxx20YYMMDDxhhmmss',1);
-
-if length(fileList)==0
-    disp('No data files found.');
-    return
-end
-
-% Load data
-data=read_HCR(fileList,data,startTime,endTime);
-
-% Check if all variables were found
-for ii=1:length(dataVars)
-    if ~isfield(data,dataVars{ii})
-        dataVars{ii}=[];
+for aa=1:length(caseStart)
+    
+    disp(['Case ',num2str(aa),' of ',num2str(length(caseStart))]);
+    
+    startTime=caseStart(aa);
+    endTime=caseEnd(aa);
+    
+    
+    %% Load data
+    data=[];
+    data.DBMHX=[];
+    data.LDR=[];
+    data.FLAG=[];
+    
+    dataVars=fieldnames(data);
+    
+    % Make list of files within the specified time frame
+    fileList=makeFileList(dataDir,startTime,endTime,'xxxxxx20YYMMDDxhhmmss',1);
+    
+    if length(fileList)==0
+        disp('No data files found.');
+        return
     end
+    
+    % Load data
+    data=read_HCR(fileList,data,startTime,endTime);
+    
+    % Check if all variables were found
+    for ii=1:length(dataVars)
+        if ~isfield(data,dataVars{ii})
+            dataVars{ii}=[];
+        end
+    end
+    
+    dataVars=dataVars(~cellfun('isempty',dataVars));
+    
+    %% Find indices where correction needs to be applied
+    medianHXend=median(data.DBMHX(760:770,:));
+    
+    corrInd=medianHXend>-96;
+    
+    data.LDR(data.FLAG>1)=nan;
+    
+    dbm=data.DBMHX(:,corrInd==1);
+    ldr=data.LDR(:,corrInd==1);
+    
+    %% Find filter indices
+    med=medianHXend(corrInd==1);
+    
+    %dbmMask=dbm>-94;
+    dbmMask=dbm>med+0.5;
+           
+    dbmMaskOut=zeros(size(dbmMask));
+    
+    for ii=1:size(dbmMask,2)
+        rayIn=dbmMask(:,ii);
+        dbmMaskOut(:,ii)=bwareaopen(rayIn,10);
+    end
+    
+    maskUse=dbmMaskOut==0 & dbmMask==1;
+    
+    filterLDR=ldr;
+    filterLDR(maskUse==1)=nan;
+    
+    LDR_masked=data.LDR;
+    LDR_masked(:,corrInd==1)=filterLDR;
+    
+    maskUsePlot=zeros(size(data.LDR));
+    maskUsePlot(:,corrInd==1)=maskUse;
+    maskUsePlot(maskUsePlot==0)=nan;
+    %% Plot
+    ylimits=[-0.5 10];
+    
+    close all
+    
+    figure('DefaultAxesFontSize',11,'position',[1,100,1800,1200]);
+    colormap jet
+    
+    ax1=subplot(3,1,1);
+    fig1=surf(data.time,data.asl./1000,data.LDR,'edgecolor','none');
+    view(2);
+    ylim(ylimits);
+    caxis([-40 -10])
+    ylabel('Altitude (km)');
+    xlim([data.time(1),data.time(end)]);
+    title('LDR')
+    
+    ax1=subplot(3,1,2);
+    fig1=surf(data.time,data.asl./1000,maskUsePlot,'edgecolor','none');
+    view(2);
+    ylim(ylimits);
+    ylabel('Altitude (km)');
+    xlim([data.time(1),data.time(end)]);
+    title('Mask')
+    
+    ax1=subplot(3,1,3);
+    fig1=surf(data.time,data.asl./1000,LDR_masked,'edgecolor','none');
+    view(2);
+    ylim(ylimits);
+    caxis([-40 -10])
+    ylabel('Altitude (km)');
+    xlim([data.time(1),data.time(end)]);
+    title('LDR masked')
+    %
+    % linkaxes([ax1,ax2,ax3],'xy');
+    
+    formatOut = 'yyyymmdd_HHMM'; set(gcf,'PaperPositionMode','auto')
+    print([figdir,datestr(startTime,formatOut),'_to_',datestr(endTime,formatOut),'_ldrMask'],...
+        '-dpng','-r0');
+    
 end
-
-dataVars=dataVars(~cellfun('isempty',dataVars));
-
-%% Find indices where correction needs to be applied
-medianHXend=median(data.DBMHX(760:770,:));
-
-corrInd=medianHXend>-100;
-
-data.LDR(data.FLAG>1)=nan;
-
-dbm=data.DBMHX(:,corrInd==1);
-ldr=data.LDR(:,corrInd==1);
-
-%% Find filter indices
-ldrMask=~isnan(ldr);
-
-ldrMaskOut=zeros(size(ldrMask));
-
-for ii=1:size(ldrMask,2)
-    rayIn=ldrMask(:,ii);    
-    ldrMaskOut(:,ii)=bwareaopen(rayIn,10);
-end
-
-ldrMaskOut=bwareaopen(ldrMaskOut,20);
-
-filterLDR=(ldrMask==1 & ldrMaskOut==0);
-%% Plot
-ylimits=[-0.5 15];
-
-close all
-
-figure('DefaultAxesFontSize',11,'position',[1,100,1800,1200]);
-
-ax1=subplot(3,1,1);
-fig1=surf(data.time,data.asl./1000,data.LDR,'edgecolor','none');
-view(2);
-ylim(ylimits);
-ylabel('Altitude (km)');
-xlim([data.time(1),data.time(end)]);
-%title('Reflectivity (all data)')
-
-ax1=subplot(3,1,2);
-fig1=surf(data.time,data.asl./1000,double(filterLDR),'edgecolor','none');
-view(2);
-ylim(ylimits);
-ylabel('Altitude (km)');
-xlim([data.time(1),data.time(end)]);
-%title('Reflectivity (all data)')
-% 
-% linkaxes([ax1,ax2,ax3],'xy');
-
-% formatOut = 'yyyymmdd_HHMM'; set(gcf,'PaperPositionMode','auto')
-% print([figdir,datestr(startTime,formatOut),'_to_',datestr(endTime,formatOut),'_echoMask'],...
-%    '-dpng','-r0');
-% 
