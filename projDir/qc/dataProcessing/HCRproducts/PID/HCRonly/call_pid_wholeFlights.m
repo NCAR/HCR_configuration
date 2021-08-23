7ae66d39836154fa76a371f69b0e6e60db082b03
mike-dixon <dixon@ucar.edu>
2021-08-23 10:21:13 -0600

    Running pid

--- projDir/qc/dataProcessing/HCRproducts/PID/HCRonly/call_pid_wholeFlights.m
+++ projDir/qc/dataProcessing/HCRproducts/PID/HCRonly/call_pid_wholeFlights.m
@@ -6,9 +6,9 @@ close all
 addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
 
 project='otrec'; %socrates, aristo, cset
-quality='qc2'; %field, qc1, or qc2
-qcVersion='v2.2';
-freqData='10hz'; % 10hz, 100hz, 2hz, or combined
+% quality='qc2'; %field, qc1, or qc2
+% qcVersion='v2.1';
+% freqData='10hz'; % 10hz, 100hz, 2hz, or combined
 whichModel='era5';
 
 saveTime=1;
@@ -31,7 +31,7 @@ infile=['~/git/HCR_configuration/projDir/qc/dataProcessing/scriptsFiles/flights_
 
 caseList = table2array(readtable(infile));
 
-for aa=5:size(caseList,1)
+for aa=1:size(caseList,1)
     disp(['Flight ',num2str(aa)]);
     disp('Loading data ...')
     
--- projDir/qc/dataProcessing/HCRproducts/stratConv/cfradial_convStrat.m
+++ projDir/qc/dataProcessing/HCRproducts/stratConv/cfradial_convStrat.m
@@ -7,7 +7,7 @@ close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-project='socrates'; %socrates, aristo, cset, otrec
+project='otrec'; %socrates, aristo, cset, otrec
 quality='qc2'; %field, qc1, or qc2
 % dataFreq='10hz';
 % qcVersion='v2.1';
@@ -38,7 +38,7 @@ caseList = table2array(readtable(infile));
 %% Run processing
 
 % Go through flights
-for ii=2:size(caseList,1)
+for ii=1:size(caseList,1)
     
     disp(['Flight ',num2str(ii)]);
     
