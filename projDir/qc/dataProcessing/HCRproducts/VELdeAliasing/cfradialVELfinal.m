c4cb61f9560a97bc20d5a2d6b49118426bc52f1f
mike-dixon <dixon@ucar.edu>
2021-09-20 13:40:10 -0600

    Adding cfradialVELfinal.m

---
+++ projDir/qc/dataProcessing/HCRproducts/VELdeAliasing/cfradialVELfinal.m
@@ -0,0 +1,107 @@
+% add VEL_FINAL data to cfradial files
+clear all;
+close all;
+
+addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
+
+project='spicule'; % socrates, cset, aristo, otrec
+quality='qc1'; % field, qc1, qc2
+qcVersion='v1.0';
+freqData='10hz';
+whichModel='era5';
+
+formatOut = 'yyyymmdd';
+
+infile=['~/git/HCR_configuration/projDir/qc/dataProcessing/scriptsFiles/flights_',project,'_data.txt'];
+
+caseList = table2array(readtable(infile));
+
+indir=HCRdir(project,quality,qcVersion,freqData);
+
+[~,modeldir]=modelDir(project,whichModel,freqData);
+
+%% Run processing
+
+% Go through flights
+for ii=1:size(caseList,1)
+    
+    disp(['Flight ',num2str(ii)]);
+    
+    startTime=datetime(caseList(ii,1:6));
+    endTime=datetime(caseList(ii,7:12));
+    
+    fileList=makeFileList(indir,startTime,endTime,'xxxxxx20YYMMDDxhhmmss',1);
+    
+    if ~isempty(fileList)
+        
+        % Get model data
+        model.velFinal=[];
+        
+        model=read_model(model,modeldir,startTime,endTime);
+        timeModelNum=datenum(model.time);
+        
+        %% Loop through HCR data files
+        for jj=1:length(fileList)
+            infile=fileList{jj};
+            
+            disp(infile);
+            
+            % Find times that are equal
+            startTimeIn=ncread(infile,'time_coverage_start')';
+            startTimeFile=datetime(str2num(startTimeIn(1:4)),str2num(startTimeIn(6:7)),str2num(startTimeIn(9:10)),...
+                str2num(startTimeIn(12:13)),str2num(startTimeIn(15:16)),str2num(startTimeIn(18:19)));
+            timeRead=ncread(infile,'time')';
+            timeHCR=startTimeFile+seconds(timeRead);
+            
+            timeHcrNum=datenum(timeHCR);
+            
+            [C,ia,ib] = intersect(timeHcrNum,timeModelNum);
+            
+            if length(timeHCR)~=length(ib)
+                warning('Times do not match up. Skipping file.')
+                continue
+            end
+            
+            % Write output
+            fillVal=-9999;
+            
+            modVars=fields(model);
+            
+            for kk=1:length(modVars)
+                if ~strcmp((modVars{kk}),'time')
+                    modOut.(modVars{kk})=model.(modVars{kk})(:,ib);
+                    modOut.(modVars{kk})(isnan(modOut.(modVars{kk})))=fillVal;
+                    modOut.(modVars{kk})=modOut.(modVars{kk});
+                end
+            end
+            
+            % Open file
+            ncid = netcdf.open(infile,'WRITE');
+            netcdf.setFill(ncid,'FILL');
+            
+            % Get dimensions
+            dimtime = netcdf.inqDimID(ncid,'time');
+            dimrange = netcdf.inqDimID(ncid,'range');
+            
+            % Define variables
+            netcdf.reDef(ncid);
+            varidVEL = netcdf.defVar(ncid,'VEL_FINAL','NC_FLOAT',[dimrange dimtime]);
+            netcdf.defVarFill(ncid,varidVEL,false,fillVal);
+            netcdf.endDef(ncid);
+            
+            % Write variables
+            netcdf.putVar(ncid,varidVEL,modOut.velFinal);
+                       
+            netcdf.close(ncid);
+            
+            % Write attributes
+            ncwriteatt(infile,'VEL_FINAL','long_name','doppler_velocity_masked_de-aliased');
+            ncwriteatt(infile,'VEL_FINAL','standard_name','radial_velocity_of_scatterers_away_from_instrument');
+            ncwriteatt(infile,'VEL_FINAL','units','m/s');
+            ncwriteatt(infile,'VEL_FINAL','comment','This field is computed by de-aliasing velocigy, and masking non-vertical pointing and non-cloud data.');
+            ncwriteatt(infile,'VEL_FINAL','grid_mapping','grid_mapping');
+            ncwriteatt(infile,'VEL_FINAL','coordinates','time range');
+                        
+        end
+    end
+end
