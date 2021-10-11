a274403f721a08207660ebbc8e88318cf0bd204a
mike-dixon <dixon@ucar.edu>
2021-10-11 23:18:06 +0000

    Plotting PID for socrates

--- projDir/qc/dataProcessing/HCRproducts/PID/HCRonly/pid_plotMat_byHour.m
+++ projDir/qc/dataProcessing/HCRproducts/PID/HCRonly/pid_plotMat_byHour.m
@@ -5,10 +5,10 @@ close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-project='otrec'; %socrates, aristo, cset, otrec
-quality='qc2'; %field, qc1, or qc2
-% dataFreq='10hz';
-% qcVersion='v2.1';
+project='socrates'; %socrates, aristo, cset, otrec
+quality='qc3'; %field, qc1, or qc2
+freqData='10hz';
+qcVersion='v3.0';
 whichModel='era5';
 
 if strcmp(project,'otrec')
@@ -21,15 +21,19 @@ end
 
 addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
 
-if strcmp(project,'otrec')
-    indir='/scr/sleet2/rsfdata/projects/otrec/hcr/qc2/cfradial/development/pid/10hz/';
-elseif strcmp(project,'socrates')
-    indir='/scr/snow2/rsfdata/projects/socrates/hcr/qc2/cfradial/development/pid/10hz/';
-end
+indir=HCRdir(project,quality,qcVersion,freqData);
+
+[~,modeldir]=modelDir(project,whichModel,quality,qcVersion,freqData);
+
+% if strcmp(project,'otrec')
+%     indir='/scr/sleet2/rsfdata/projects/otrec/hcr/qc2/cfradial/development/pid/10hz/';
+% elseif strcmp(project,'socrates')
+%     indir='/scr/snow2/rsfdata/projects/socrates/hcr/qc2/cfradial/development/pid/10hz/';
+% end
+% 
+% modeldir=[indir(1:end-30),'mat/pid/10hz/'];
 
 figdir=[indir(1:end-5),'pidPlots/wholeFlights/'];
-
-modeldir=[indir(1:end-30),'mat/pid/10hz/'];
 
 infile=['~/git/HCR_configuration/projDir/qc/dataProcessing/scriptsFiles/flights_',project,'_data.txt'];
 
