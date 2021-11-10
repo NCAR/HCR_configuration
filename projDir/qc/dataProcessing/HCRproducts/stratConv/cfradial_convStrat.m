52d0b83003156e5e636de8d6c37ae46cc810ef2b
mike-dixon <dixon@ucar.edu>
2021-11-10 14:18:13 -0700

    Setting up HawkEye for SPICULE QC1.1

--- projDir/qc/dataProcessing/HCRproducts/stratConv/cfradial_convStrat.m
+++ projDir/qc/dataProcessing/HCRproducts/stratConv/cfradial_convStrat.m
@@ -7,10 +7,10 @@ close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-project='otrec'; %socrates, aristo, cset, otrec
-quality='qc3'; %field, qc1, or qc2
+project='spicule'; %socrates, aristo, cset, otrec
+quality='qc1'; %field, qc1, or qc2
 freqData='10hz';
-qcVersion='v3.0';
+qcVersion='v1.1';
 whichModel='era5';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
