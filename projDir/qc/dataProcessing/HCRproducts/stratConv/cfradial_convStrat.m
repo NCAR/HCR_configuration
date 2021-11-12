6e41a9a0e50e5e9afb25be2705aa86fb4a06300c
mike-dixon <dixon@ucar.edu>
2021-11-12 22:20:29 +0000

    Setting up combined

--- projDir/qc/dataProcessing/HCRproducts/stratConv/cfradial_convStrat.m
+++ projDir/qc/dataProcessing/HCRproducts/stratConv/cfradial_convStrat.m
@@ -7,10 +7,10 @@ close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-project='spicule'; %socrates, aristo, cset, otrec
-quality='qc1'; %field, qc1, or qc2
+project='cset'; %socrates, aristo, cset, otrec
+quality='qc3'; %field, qc1, or qc2
 freqData='10hz';
-qcVersion='v1.1';
+qcVersion='v3.0';
 whichModel='era5';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
