20b0a607304fb6bf391215ce1df78b914e36f939
mike-dixon <dixon@ucar.edu>
2021-12-11 00:28:17 +0000

    Creating 2hz combined for CSET

--- projDir/qc/dataProcessing/utils/HCRdir.m
+++ projDir/qc/dataProcessing/utils/HCRdir.m
@@ -66,8 +66,10 @@ elseif strcmp(project,'cset')
             return
         end
     elseif strcmp(qc,'qc3')
-        if strcmp(freq,'10hz')
+        if strcmp(freq,'10hz') | strcmp(freq,'2hz')
             indir=['/scr/snow2/rsfdata/projects/cset/hcr/',qc,'/cfradial/',qcVersion,'_full/',freq,'/'];
+        elseif strcmp(freq,'combined')
+            indir=['/scr/snow2/rsfdata/projects/cset/hcr/',qc,'/cfradial/hcr_hsrl_merge/',qcVersion,'/2hz/'];
         else
             disp('The requested data does not exist.');
             return
