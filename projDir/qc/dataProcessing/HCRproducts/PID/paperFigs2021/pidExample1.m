26ef9647e5eaf4cb6bef1ca6c6af13fa3e7fe305
mike-dixon <dixon@ucar.edu>
2021-12-21 00:39:58 +0000

    Small changes to figure

--- projDir/qc/dataProcessing/HCRproducts/PID/paperFigs2021/pidExample1.m
+++ projDir/qc/dataProcessing/HCRproducts/PID/paperFigs2021/pidExample1.m
@@ -29,6 +29,7 @@ data.HCR_LDR=[];
 data.TEMP=[];
 data.HCR_ICING_LEVEL=[];
 data.PID=[];
+data.HCR_PID=[];
 data.HSRL_Aerosol_Backscatter_Coefficient=[];
 data.HSRL_Particle_Linear_Depolarization_Ratio=[];
 
@@ -65,7 +66,7 @@ disp('Plotting PID');
 close all
 
 wi=10;
-hi=9.5;
+hi=10.5;
 
 fig1=figure('DefaultAxesFontSize',11,'DefaultFigurePaperType','<custom>','units','inch','position',[3,100,wi,hi]);
 fig1.PaperPositionMode = 'manual';
@@ -78,7 +79,7 @@ fig1.InvertHardcopy = 'off';
 
 set(fig1,'color','w');
 
-s1=subplot(7,1,1);
+s1=subplot(8,1,1);
 surf(data.time,data.asl./1000,data.HCR_DBZ,'edgecolor','none');
 view(2);
 ylim(ylimits);
@@ -95,7 +96,7 @@ box on
 s1.XTickLabel=[];
 s1.YTick=0:0.5:1.5;
 
-s2=subplot(7,1,2);
+s2=subplot(8,1,2);
 surf(data.time,data.asl./1000,data.HCR_VEL,'edgecolor','none');
 view(2);
 ylim(ylimits);
@@ -113,7 +114,7 @@ box on
 s2.XTickLabel=[];
 s2.YTick=0:0.5:1.5;
 
-s3=subplot(7,1,3);
+s3=subplot(8,1,3);
 surf(data.time,data.asl./1000,data.HCR_LDR,'edgecolor','none');
 view(2);
 ylim(ylimits);
@@ -130,7 +131,7 @@ box on
 s3.XTickLabel=[];
 s3.YTick=0:0.5:1.5;
 
-s4=subplot(7,1,4);
+s4=subplot(8,1,4);
 surf(data.time,data.asl./1000,data.HSRL_Particle_Linear_Depolarization_Ratio,'edgecolor','none');
 view(2);
 ylim(ylimits);
@@ -148,7 +149,7 @@ box on
 s4.XTickLabel=[];
 s4.YTick=0:0.5:1.5;
 
-s5=subplot(7,1,5);
+s5=subplot(8,1,5);
 surf(data.time,data.asl./1000,log10(data.HSRL_Aerosol_Backscatter_Coefficient),'edgecolor','none');
 view(2);
 ylim(ylimits);
@@ -165,7 +166,7 @@ box on
 s5.XTickLabel=[];
 s5.YTick=0:0.5:1.5;
 
-s6=subplot(7,1,6);
+s6=subplot(8,1,6);
 hold on
 surf(data.time,data.asl./1000,data.TEMP,'edgecolor','none');
 view(2);
@@ -185,7 +186,7 @@ box on
 s6.XTickLabel=[];
 s6.YTick=0:0.5:1.5;
 
-s7=subplot(7,1,7);
+s7=subplot(8,1,7);
 surf(data.time,data.asl./1000,data.PID,'edgecolor','none');
 view(2);
 ylim(ylimits);
@@ -196,39 +197,61 @@ cb7=colorbar;
 cb7.Ticks=1:11;
 cb7.TickLabels=units_str_hcr;
 ylabel('Altitude (km)');
-text(startTime+seconds(5),ylimits(2)-0.2,'(g) PID',...
+s7.XTickLabel=[];
+text(startTime+seconds(5),ylimits(2)-0.2,'(g) PID combined',...
     'fontsize',11,'fontweight','bold','BackgroundColor','w','Margin',0.5);
 grid on
 box on
 s7.YTick=0:0.5:1.5;
 
-s1.Position=[0.059 0.862 0.79 0.13];
-cb1.Position=[0.856 0.862 0.023 0.13];
+s8=subplot(8,1,8);
+surf(data.time,data.asl./1000,data.HCR_PID,'edgecolor','none');
+view(2);
+ylim(ylimits);
+xlim([data.time(1),data.time(end)]);
+caxis([.5 11.5]);
+colormap(s8,cscale_hcr);
+cb8=colorbar;
+cb8.Ticks=1:11;
+cb8.TickLabels=units_str_hcr;
+ylabel('Altitude (km)');
+text(startTime+seconds(5),ylimits(2)-0.2,'(h) PID HCR stand-alone',...
+    'fontsize',11,'fontweight','bold','BackgroundColor','w','Margin',0.5);
+grid on
+box on
+s8.YTick=0:0.5:1.5;
+
+s1.Position=[0.059 0.88 0.79 0.11];
+cb1.Position=[0.856 0.88 0.023 0.11];
 cb1.FontSize=9;
 
-s2.Position=[0.059 0.725 0.79 0.13];
-cb2.Position=[0.856 0.725 0.023 0.13];
+s2.Position=[0.059 0.76 0.79 0.11];
+cb2.Position=[0.856 0.76 0.023 0.11];
 cb2.FontSize=9;
 
-s3.Position=[0.059 0.588 0.79 0.13];
-cb3.Position=[0.856 0.588 0.023 0.13];
+s3.Position=[0.059 0.64 0.79 0.11];
+cb3.Position=[0.856 0.64 0.023 0.11];
 cb3.FontSize=9;
 
-s4.Position=[0.059 0.451 0.79 0.13];
-cb4.Position=[0.856 0.451 0.023 0.13];
+s4.Position=[0.059 0.52 0.79 0.11];
+cb4.Position=[0.856 0.52 0.023 0.11];
 cb4.FontSize=9;
 
-s5.Position=[0.059 0.314 0.79 0.13];
-cb5.Position=[0.856 0.314 0.023 0.13];
+s5.Position=[0.059 0.4 0.79 0.11];
+cb5.Position=[0.856 0.4 0.023 0.11];
 cb5.FontSize=9;
 
-s6.Position=[0.059 0.177 0.79 0.13];
-cb6.Position=[0.856 0.177 0.023 0.13];
+s6.Position=[0.059 0.28 0.79 0.11];
+cb6.Position=[0.856 0.28 0.023 0.11];
 cb6.FontSize=9;
 
-s7.Position=[0.059 0.04 0.79 0.13];
-cb7.Position=[0.856 0.04 0.023 0.13];
+s7.Position=[0.059 0.16 0.79 0.11];
+cb7.Position=[0.856 0.16 0.023 0.11];
 cb7.FontSize=9;
+
+s8.Position=[0.059 0.04 0.79 0.11];
+cb8.Position=[0.856 0.04 0.023 0.11];
+cb8.FontSize=9;
 
 set(gcf,'PaperPositionMode','auto')
 print(fig1,[figdir,'pidExample1.png'],'-dpng','-r0')
