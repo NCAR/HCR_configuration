02924e03d2d2c7cca95c291551ed0fef7c47ae15
mike-dixon <dixon@ucar.edu>
2022-12-27 18:08:03 -0700

    Re-organizing cloud property paper

---
+++ projDir/qc/dataProcessing/research/cloudProps/paperFigs/csetDims.m
@@ -0,0 +1,175 @@
+% Call cloud puzzle script
+
+clear all;
+close all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+projects={'cset','socrates','otrec'}; %socrates, aristo, cset, otrec
+freqData='10hz';
+whichModel='era5';
+
+figdir='/scr/snow2/rsfdata/projects/cset/hcr/qc3/cfradial/v3.0_full/cloudPropsProjects/paperFigs/';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
+
+for ii=1:length(projects)
+    if strcmp(projects{ii},'cset')
+        qcVersion='v3.0';
+        quality='qc3';
+    else
+        qcVersion='v3.1';
+        quality='qc3';
+    end
+
+    cfDir=HCRdir(projects{ii},quality,qcVersion,freqData);
+
+    indir=[cfDir(1:end-5),'cloudProps/'];
+
+    in.(projects{ii})=load([indir,projects{ii},'_cloudProps.mat']);
+end
+
+plotVars=fields(in.cset);
+
+for ii=1:length(plotVars)
+    for jj=1:length(projects)
+        if ~(strcmp(projects{jj},'spicule') & strcmp(plotVars{ii},'sstAll'))
+            plotV.(plotVars{ii}).(projects{jj})=in.(projects{jj}).(plotVars{ii});
+        end
+    end
+end
+
+%% Plot
+
+disp('Plotting ...')
+
+
+%% Plot
+
+lonLims=[-160,-120;
+    130,165;
+    -95,-75];
+
+latLims=[15,45;
+    -65,-40
+    -0,15];
+
+close all
+
+
+fig=figure('DefaultAxesFontSize',11,'position',[100,100,600,1100],'visible','on','renderer','painters');
+
+load coastlines
+
+% (a) Cloud base
+
+s1=subplot(3,1,1);
+
+hold on
+
+thisLons=plotV.lonAll.cset.CloudLow;
+
+thisLats=plotV.latAll.cset.CloudLow;
+
+plotVar=plotV.cloudBaseAll.cset.CloudLow;
+
+scatter(thisLons,thisLats,25,plotVar,'filled');
+
+caxis([0 3]);
+s1.Colormap=jet;
+cb1=colorbar;
+
+xlim(lonLims(1,:));
+ylim(latLims(1,:));
+
+plot(coastlon,coastlat,'-k')
+title('(a) Cloud base low clouds (km)');
+
+xlabel('Longitude (deg)')
+ylabel('Latitude (deg)')
+
+grid on
+box on
+
+% (b) Cloud top
+
+s2=subplot(3,1,2);
+
+hold on
+
+thisLons=cat(1,plotV.lonAll.cset.CloudLow,plotV.lonAll.cset.ConvYoungShallow, ...
+    plotV.lonAll.cset.ConvMatureShallow,plotV.lonAll.cset.StratShallow);
+
+thisLats=cat(1,plotV.latAll.cset.CloudLow,plotV.latAll.cset.ConvYoungShallow, ...
+    plotV.latAll.cset.ConvMatureShallow,plotV.latAll.cset.StratShallow);
+
+plotVar=cat(1,plotV.cloudTopAll.cset.CloudLow,plotV.cloudTopAll.cset.ConvYoungShallow, ...
+    plotV.cloudTopAll.cset.ConvMatureShallow,plotV.cloudTopAll.cset.StratShallow);
+
+scatter(thisLons,thisLats,25,plotVar,'filled');
+
+caxis([0 4]);
+s2.Colormap=jet;
+cb2=colorbar;
+
+xlim(lonLims(1,:));
+ylim(latLims(1,:));
+
+plot(coastlon,coastlat,'-k')
+title('(b) Cloud top shallow and low clouds (km)');
+
+xlabel('Longitude (deg)')
+ylabel('Latitude (deg)')
+
+grid on
+box on
+
+% (c) Cloud width
+
+s3=subplot(3,1,3);
+
+hold on
+
+thisLons=cat(1,plotV.lonAll.cset.CloudLow,plotV.lonAll.cset.ConvYoungShallow, ...
+    plotV.lonAll.cset.ConvMatureShallow,plotV.lonAll.cset.StratShallow);
+
+thisLats=cat(1,plotV.latAll.cset.CloudLow,plotV.latAll.cset.ConvYoungShallow, ...
+    plotV.latAll.cset.ConvMatureShallow,plotV.latAll.cset.StratShallow);
+
+plotVar=cat(1,plotV.cloudLengthAll.cset.CloudLow,plotV.cloudLengthAll.cset.ConvYoungShallow, ...
+    plotV.cloudLengthAll.cset.ConvMatureShallow,plotV.cloudLengthAll.cset.StratShallow);
+
+% thisLons(plotVar<20)=[];
+% thisLats(plotVar<20)=[];
+% plotVar(plotVar<20)=[];
+
+scatter(thisLons,thisLats,25,plotVar,'filled');
+
+caxis([0 80]);
+s3.Colormap=jet;
+cb3=colorbar;
+
+xlim(lonLims(1,:));
+ylim(latLims(1,:));
+
+plot(coastlon,coastlat,'-k')
+title('(c) Cloud width shallow and low clouds (km)');
+
+xlabel('Longitude (deg)')
+ylabel('Latitude (deg)')
+
+grid on
+box on
+
+s1.Position=[0.095,0.71,0.8,0.27];
+s2.Position=[0.095,0.377,0.8,0.27];
+s3.Position=[0.095,0.045,0.8,0.27];
+
+cb1.Position=[0.915,0.71,0.035,0.27];
+cb2.Position=[0.915,0.377,0.035,0.27];
+cb3.Position=[0.915,0.045,0.035,0.27];
+
+set(gcf,'PaperPositionMode','auto')
+print([figdir,'csetDims.png'],'-dpng','-r0');
--- projDir/qc/dataProcessing/research/cloudProps/paperFigs/csetLocs.m
+++ projDir/qc/dataProcessing/research/cloudProps/paperFigs/csetLocs.m
@@ -1,11 +1,11 @@
-% Plot OTREC locations
+% Plot cset locations
 
 clear all;
 close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-project={'otrec'}; %socrates, aristo, cset, otrec
+project={'cset'}; %socrates, aristo, cset, otrec
 freqData='10hz';
 whichModel='era5';
 
@@ -16,7 +16,7 @@ figdir='/scr/snow2/rsfdata/projects/cset/hcr/qc3/cfradial/v3.0_full/cloudPropsPr
 addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
 
 
-qcVersion='v3.1';
+qcVersion='v3.0';
 quality='qc3';
 ii=1;
 
@@ -26,7 +26,7 @@ indir=[cfDir(1:end-5),'cloudProps/'];
 
 in.(project{ii})=load([indir,project{ii},'_cloudProps.mat']);
 
-plotVars=fields(in.otrec);
+plotVars=fields(in.cset);
 
 for ii=1:length(plotVars)
     for jj=1:length(project)
@@ -45,10 +45,10 @@ classTypes={'CloudLow','CloudMid','CloudHigh',...
     'ConvYoungShallow','ConvYoungMid','ConvYoungDeep',...
     'ConvMatureShallow','ConvMatureMid','ConvMatureDeep'};
 
-classTypeNames={'(a) CloudLow','(b) CloudMid','(c) CloudHigh',...
-    '(j) StratShallow','(k) StratMid','(l) StratDeep',...
-    '(d) ConvShallow','(e) ConvMid','(f) ConvDeep',...
-    '(g) ConvStratShallow','(h) ConvStratMid','(i) ConvStratDeep'};
+classTypeNames={'(a) CloudLow','(b) CloudMid','(e) CloudHigh',...
+    '(d) StratShallow','(k) StratMid','(l) StratDeep',...
+    '(b) ConvShallow','(e) ConvMid','(f) ConvDeep',...
+    '(c) ConvStratShallow','(h) ConvStratMid','(i) ConvStratDeep'};
 
 close all
 
@@ -73,15 +73,15 @@ load coastlines
 %% Per flight hour
 load('/scr/snow2/rsfdata/projects/cset/hcr/qc3/cfradial/v3.0_full/cloudPropsProjects/flightHourGrids.mat');
 
-fig=figure('DefaultAxesFontSize',11,'position',[100,100,1200,1000],'renderer','painters','visible','on');
-colmap=turbo(13);
-colormap(colmap(2:end,:));
+fig=figure('DefaultAxesFontSize',11,'position',[100,10,600,1270],'renderer','painters','visible','on');
+colmap=turbo(21);
+colormap(colmap(2:end-2,:));
 
 % Loop through projects
 projects=fields(lons);
 gridStep=1;
 
-bb=3;
+bb=1;
 
 lonLength=(lonLims(bb,2)-lonLims(bb,1))/gridStep;
 latLength=(latLims(bb,2)-latLims(bb,1))/gridStep;
@@ -91,16 +91,16 @@ latSteps=latLims(bb,1):gridStep:latLims(bb,2);
 
 plotNum=num2str(1:12);
 
-nums=[1,2,3,7,8,9,10,11,12,4,5,6];
+nums=[1,7,10,4,3];
 
-for aa=1:length(classTypes)
+for aa=1:length(nums)
     hourGrid=zeros(latLength,lonLength);
-    s.plotNum{aa}=subplot(4,3,aa);
+    s.plotNum{aa}=subplot(5,1,aa);
 
     hold on
 
-    thisLons=lons.otrec.(classTypes{nums(aa)});
-    thisLats=lats.otrec.(classTypes{nums(aa)});
+    thisLons=lons.cset.(classTypes{nums(aa)});
+    thisLats=lats.cset.(classTypes{nums(aa)});
 
     % Loop through output grid
     for ii=1:size(hourGrid,1)
@@ -112,57 +112,47 @@ for aa=1:length(classTypes)
         end
     end
 
-    perHour=hourGrid./flightHourGrids.otrec;
+    perHour=hourGrid./flightHourGrids.cset;
     perHour(perHour==0)=nan;
-    perHour(flightHourGrids.otrec<0.5)=nan;
+    perHour(flightHourGrids.cset<0.167)=nan;
 
     h=imagesc(lonSteps(1:end-1)+gridStep/2,latSteps(1:end-1)+gridStep/2,perHour);
     set(h,'AlphaData',~isnan(perHour));
 
-    caxis([0,12]);
-    if aa==12
+    caxis([0,18]);
+    if aa==5
         cb=colorbar;
     end
 
-    if aa==2 | aa==3 | aa==5 | aa==6 | aa==8 | aa==9 | aa==11 | aa==12
-        s.plotNum{aa}.YTickLabel=[];
-    else
-        ylabel('Latitude (deg)');
-        %yticklabels({'-95','-90','-85','-80'});
-    end
-    if aa<10
+    ylabel('Latitude (deg)');
+    yticks(15:5:45);
+    %yticklabels({'-95','-90','-85','-80'});
+
+    if aa<5
         s.plotNum{aa}.XTickLabel=[];
     else
         xlabel('Longitude (deg)');
-        xticks(-95:5:-80);
-        xticklabels({'-95','-90','-85','-80'});
+%         xticks(-95:5:-80);
+%         xticklabels({'-95','-90','-85','-80'});
     end
 
-    xlim(lonLims(bb,:));
-    ylim(latLims(bb,:));
+    xlim([-160,-120]);
+    ylim([18,44]);
 
     plot(coastlon,coastlat,'-k')
-    text(-94.5,13.5,[classTypeNames{nums(aa)}],'fontsize',12,'FontWeight','bold','BackgroundColor','w');
+    text(-159,42,[classTypeNames{nums(aa)}],'fontsize',12,'FontWeight','bold','BackgroundColor','w');
 
     grid on
     box on
 end
 
-s.plotNum{1}.Position=[0.042 0.765 0.295 0.225];
-s.plotNum{2}.Position=[0.3475 0.765 0.295 0.225];
-s.plotNum{3}.Position=[0.655 0.765 0.295 0.225];
+s.plotNum{1}.Position=[0.1 0.806 0.8 0.185];
+s.plotNum{2}.Position=[0.1 0.614 0.8 0.185];
+s.plotNum{3}.Position=[0.1 0.422 0.8 0.185];
+s.plotNum{4}.Position=[0.1 0.23 0.8 0.185];
+s.plotNum{5}.Position=[0.1 0.038 0.8 0.185];
 
-s.plotNum{4}.Position=[0.042 0.525 0.295 0.225];
-s.plotNum{5}.Position=[0.3475 0.525 0.295 0.225];
-s.plotNum{6}.Position=[0.655 0.525 0.295 0.225];
-
-s.plotNum{7}.Position=[0.042 0.285 0.295 0.225];
-s.plotNum{8}.Position=[0.3475 0.285 0.295 0.225];
-s.plotNum{9}.Position=[0.655 0.285 0.295 0.225];
-
-s.plotNum{10}.Position=[0.042 0.045 0.295 0.225];
-s.plotNum{11}.Position=[0.3475 0.045 0.295 0.225];
-s.plotNum{12}.Position=[0.655 0.045 0.295 0.225];
+cb.Position=[0.92,0.037,0.035,0.185];
 
 set(gcf,'PaperPositionMode','auto')
-print([figdir,'otrec_locs.png'],'-dpng','-r0');
+print([figdir,'cset_locs.png'],'-dpng','-r0');
