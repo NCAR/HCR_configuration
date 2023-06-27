dbef3f4fd47dd0727bb359d19acb7346cda479c6
mike-dixon <dixon@ucar.edu>
2023-06-27 12:33:47 -0600

    Re-Process OTREC

--- projDir/qc/dataProcessing/HCRproducts/PID/HCRonly/cfradial_pid.m
+++ projDir/qc/dataProcessing/HCRproducts/PID/HCRonly/cfradial_pid.m
@@ -5,10 +5,10 @@ close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Input variables %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-project='noreaster'; %socrates, aristo, cset, otrec
-quality='qc2'; %field, qc1, or qc2
+project='socrates'; %socrates, aristo, cset, otrec
+quality='qc3'; %field, qc1, or qc2
 freqData='10hz';
-qcVersion='v2.1';
+qcVersion='v3.2';
 whichModel='era5';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
