3185a35bd75bc1a64ce053ffbb76e2cf7612e7ea
mike-dixon <dixon@ucar.edu>
2023-10-23 17:24:26 -0600

    Adding downloadERA5.pressLev.cset

--- projDir/qc/dataProcessing/HCRproducts/modelData/model2HCR4d.m
+++ projDir/qc/dataProcessing/HCRproducts/modelData/model2HCR4d.m
@@ -4,9 +4,9 @@ close all;
 
 addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
 
-project='otrec'; % socrates, cset, aristo, otrec
+project='cset'; % socrates, cset, aristo, otrec
 quality='qc3'; % field, qc0, qc1, qc2
-qcVersion='v3.2';
+qcVersion='v3.1';
 freqData='10hz'; % 10hz, 100hz, or 2hz
 whichModel='era5'; % ecmwf or era5 or narr
 
