3a59285a6abf36dd164953e452832652cca6e7e0
dixon <dixon@ucar.edu>
2023-11-06 17:36:23 -0700

    Plotting spectra

--- projDir/qc/dataProcessing/timeSeries/calcMoments.m
+++ projDir/qc/dataProcessing/timeSeries/calcMoments.m
@@ -14,17 +14,27 @@ dataDirTS=HCRdir(project,quality,qcVersion,freqData);
 
 figdir=[dataDirTS,'figsTS/'];
 
+showPlot='on';
+ylimUpper=5.2;
+ylimLower=-0.1;
+plotSpectra=1;
+
 casefile=['~/git/HCR_configuration/projDir/qc/dataProcessing/HCRproducts/caseFiles/calcMoments_',project,'.txt'];
+if plotSpectra
+    sTimeFile=['~/git/HCR_configuration/projDir/qc/dataProcessing/HCRproducts/caseFiles/spectraTimes_',project,'.txt'];
+end
 
 freqStr=strfind(freqData,'hz');
 outFreq=str2num(freqData(1:freqStr-1)); % Desired output frequency in Hz
 timeSpan=1/outFreq;
 
-showPlot='on';
-ylimUpper=5.2;
-ylimLower=-0.1;
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if plotSpectra
+    plotTimeRange=readtable(sTimeFile);
+    plotTimes=datetime(plotTimeRange.Var1,plotTimeRange.Var2,plotTimeRange.Var3, ...
+        plotTimeRange.Var4,plotTimeRange.Var5,plotTimeRange.Var6);
+    plotRangeKM=plotTimeRange.Var7;
+end
 
 % Loop through cases
 
@@ -34,7 +44,7 @@ caseStart=datetime(caseList.Var1,caseList.Var2,caseList.Var3, ...
 caseEnd=datetime(caseList.Var7,caseList.Var8,caseList.Var9, ...
     caseList.Var10,caseList.Var11,caseList.Var12);
 
-for aa=2:length(caseStart)
+for aa=1:length(caseStart)
 
     disp(['Case ',num2str(aa),' of ',num2str(length(caseStart))]);
 
@@ -130,6 +140,63 @@ for aa=2:length(caseStart)
             flipYes=0;
         end
 
+        %% Plot spectra
+        if plotSpectra
+            close all
+            plotTimeDiff=abs(etime(datevec(data.time(startInd)),datevec(plotTimes)));
+            plotInd=find(plotTimeDiff<0.04);
+            if ~isempty(plotInd)
+                % X-axis
+                specVelVec=-pi:2*pi/(sampleNum):pi;
+                specVelVec=specVelVec(1:end-1);
+                vel=data.lambda./(4.*pi.*data.prtThis).*specVelVec;
+
+                % Find index of specified range
+                rangeInd=min(find((data.range./1000)>=plotRangeKM(plotInd)));
+
+                % Create figure
+                f0=figure('Position',[200 500 800 1200],'DefaultAxesFontSize',12);
+
+                % Plot spectra at specified range
+                s1=subplot(3,1,1);
+                plot(vel,specPowerDB.V(rangeInd,:),'-b','LineWidth',2);
+                xlabel('Velocity (m s^{-1})');
+                ylabel('Power (dB)');
+                xlim([vel(1) vel(end)]);
+                title({[datestr(data.time(startInd),'yyyy-mm-dd HH:MM:SS')];['Power at ',num2str(plotRangeKM(plotInd)),' km range.']});
+
+                text(vel(end)+0.3,-60,{['WIDTH: ',num2str(momentsSpec.width(rangeInd,ii),3),' m s^{-1}']; ...
+                    ['SKEW: ',num2str(momentsSpec.skew(rangeInd,ii),3)]; ...
+                    ['KURT: ',num2str(momentsSpec.kurt(rangeInd,ii),3)]});
+
+                % Waterfall plot
+                s2=subplot(3,1,2:3);
+                colormap('jet');
+                hold on
+                surf(vel,data.range./1000,specPowerDB.V,'EdgeColor','none');
+                view(2)
+                caxis([-60 -30]);
+                colorbar
+                xlabel('Velocity (m s^{-1})');
+                ylabel('Range (km)');
+                xlim([vel(1) vel(end)])
+                ylim([data.range(1)./1000,ylimUpper])
+                title('Power (dB)');
+
+                % Plot arrow at specified range
+                text(vel(1)-1.5,plotRangeKM(plotInd),'\rightarrow','FontSize',20,'color','b','FontWeight','bold','VerticalAlignment','middle');
+
+                % Align x axis of subplots
+                drawnow;
+                s1Pos=s1.Position;
+                s2Pos=s2.Position;
+                s1.Position=[s1Pos(1),s1Pos(2),s2Pos(3),s1Pos(4)];
+
+                set(gcf,'PaperPositionMode','auto')
+                print(f0,[figdir,project,'_spectra_',datestr(data.time(startInd),'yyyymmdd_HHMMSS'),'_',num2str(plotRangeKM(plotInd)),'km.png'],'-dpng','-r0');
+            end
+        end
+
         %% Next beam
         startInd=endInd+1;
         ii=ii+1;
@@ -143,6 +210,6 @@ for aa=2:length(caseStart)
 
     plotMomentsCompare(data,momentsTime,timeBeams,figdir,project,'Time',ylimUpper,flipYes,showPlot);
     plotMomentsCompare(data,momentsSpec,timeBeams,figdir,project,'Spec',ylimUpper,flipYes,showPlot);
-    
+
     plotMomentsDiff(data,momentsTime,momentsSpec,timeBeams,figdir,project,ylimUpper,flipYes,showPlot);
 end
