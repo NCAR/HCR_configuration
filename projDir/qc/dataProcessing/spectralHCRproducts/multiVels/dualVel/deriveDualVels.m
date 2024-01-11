f3e7b0c2c85dda3efce91d351da4d526314c465b
RSF Data Group <rsfdata@eol-virga.eol.ucar.edu>
2024-01-10 17:17:44 -0700

    New philosophy for multi vel retrieval

---
+++ projDir/qc/dataProcessing/spectralHCRproducts/multiVels/dualVel/deriveDualVels.m
@@ -0,0 +1,323 @@
+% Analyze HCR time series
+clear all;
+close all;
+
+addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
+
+project='spicule'; %socrates, aristo, cset, otrec
+quality='ts'; %field, qc1, or qc2
+qualityCF=[];
+freqData=[];
+qcVersion=[];
+
+outTime=0.1; % Desired output time resolution in seconds. Must be less than or equal to one second.
+sampleTime=0.02; % Length of sample in seconds.
+
+dataDirTS=HCRdir(project,quality,qcVersion,freqData);
+
+figdir=[dataDirTS(1:end-6),'figsDualVel/cases/'];
+
+showPlot='on';
+
+casefile=['~/git/HCR_configuration/projDir/qc/dataProcessing/HCRproducts/caseFiles/dualParticles_',project,'.txt'];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Loop through cases
+
+caseList=readtable(casefile);
+caseStart=datetime(caseList.Var1,caseList.Var2,caseList.Var3, ...
+    caseList.Var4,caseList.Var5,caseList.Var6);
+caseEnd=datetime(caseList.Var7,caseList.Var8,caseList.Var9, ...
+    caseList.Var10,caseList.Var11,caseList.Var12);
+
+for aa=1:length(caseStart)
+    tic
+
+    disp(['Case ',num2str(aa),' of ',num2str(length(caseStart))]);
+
+    startTime=caseStart(aa);
+    endTime=caseEnd(aa);
+
+    fileListTS=makeFileList(dataDirTS,startTime+seconds(1),endTime-seconds(1),'20YYMMDDxhhmmss',1);
+
+    if isempty(fileListTS)
+        warning('No data found.');
+        continue
+    end
+
+    for bb=1:length(fileListTS)
+
+        if bb==1
+            data=[];
+            data.IVc=[];
+            data.QVc=[];
+            %data.IHx=[];
+            %data.QHx=[];
+            data.eastward_velocity=[];
+            data.northward_velocity=[];
+            data.vertical_velocity=[];
+            data.azimuth_vc=[];
+
+            vars=fieldnames(data);
+
+            %% Load data TS
+            disp(['Loading time series file ',num2str(bb),' of ' num2str(length(fileListTS)),' ...']);
+            data=readHCRts(fileListTS(1),data,startTime-seconds(outTime),endTime+seconds(outTime),0);
+
+            % Find available times
+            timeTest=data.time';
+            timeTest(data.time<startTime | data.time>endTime)=[];
+            TTdata=timetable(timeTest,ones(size(timeTest)));
+            synchTT=retime(TTdata,'regular','sum','TimeStep',seconds(outTime));
+            goodTimes=synchTT.timeTest(synchTT.Var1>0);
+
+            beamNum=length(goodTimes)-1;
+
+            % Set up de-aliasing
+            defaultPrev=data.lambda/(mode(data.prt)*4);
+
+            velPrev=repmat(defaultPrev,size(data.range,1),1);
+            prevCount=zeros(size(velPrev));
+            prevKeep=nan(size(velPrev));
+            flipYes=0;
+        else
+            %% Trimm first file
+            data=trimFirstFile(data,endInd);
+
+            goodTimes(1:end-1)=[];
+
+            %% Load data TS
+            disp(['Loading time series file ',num2str(bb),' of ' num2str(length(fileListTS)),' ...']);
+
+            data=readHCRts_add(fileListTS{bb},data,vars);
+
+            % Find available times
+            timeTest=data.time';
+            timeTest(data.time<goodTimes | data.time>endTime)=[];
+        end
+
+        TTdata=timetable(timeTest,ones(size(timeTest)));
+        synchTT=retime(TTdata,'regular','sum','TimeStep',seconds(outTime));
+        goodTimes=synchTT.timeTest(synchTT.Var1>0);
+
+        if bb<length(fileListTS)
+            beamNum=length(goodTimes)-1;
+        else
+            beamNum=length(goodTimes);
+        end
+
+        %% Calculate moments
+
+        disp('Calculating moments ...')
+        momentsTimeOne.powerV=nan(size(data.range,1),beamNum);
+        momentsTimeOne.powerH=nan(size(data.range,1),beamNum);
+        momentsTimeOne.velRaw=nan(size(data.range,1),beamNum);
+        momentsTimeOne.velRawDeAliased=nan(size(data.range,1),beamNum);
+        momentsTimeOne.vel=nan(size(data.range,1),beamNum);
+        momentsTimeOne.width=nan(size(data.range,1),beamNum);
+        momentsTimeOne.dbz=nan(size(data.range,1),beamNum);
+        momentsTimeOne.snr=nan(size(data.range,1),beamNum);
+        momentsTimeOne.skew=nan(size(data.range,1),beamNum);
+        momentsTimeOne.kurt=nan(size(data.range,1),beamNum);
+        momentsTimeOne.ldr=nan(size(data.range,1),beamNum);
+        momentsTimeOne.ncp=nan(size(data.range,1),beamNum);
+        momentsTimeOne.range=nan(size(data.range,1),beamNum);
+        momentsTimeOne.altitude=nan(1,beamNum);
+        momentsTimeOne.elevation=nan(1,beamNum);
+        momentsTimeOne.eastward_velocity=nan(1,beamNum);
+        momentsTimeOne.northward_velocity=nan(1,beamNum);
+        momentsTimeOne.vertical_velocity=nan(1,beamNum);
+        momentsTimeOne.azimuth_vc=nan(1,beamNum);
+        momentsTimeOne.time=goodTimes(1:beamNum)';
+
+        momentsVelDualRawOne=single(nan(size(data.range,1),beamNum,1));
+        momentsPowerDualRawOne=single(nan(size(data.range,1),beamNum,1));
+
+        % Loop through beams
+        for ii=1:beamNum
+
+            %disp(datestr(goodTimes(ii),'yyyymmdd_HHMMSS.FFF'));
+            
+            % Find start and end indices for beam
+            [~,startInd]=min(abs(goodTimes(ii)-seconds(sampleTime/2)-data.time));
+            [~,endInd]=min(abs(goodTimes(ii)+seconds(sampleTime/2)-data.time));
+
+            sampleNum=endInd-startInd+1;
+
+            % Window
+            win=window(@hamming,sampleNum);  % Default window is Hamming
+            winWeight=sampleNum/sum(win);
+            winNorm=win*winWeight;
+
+            % Trim data down to current beam
+            dataThis=trimData(data,startInd,endInd);
+
+            % IQ
+            cIQ.v=winNorm'.*(dataThis.IVc+i*dataThis.QVc)./sqrt(sampleNum);
+            if isfield(dataThis,'IHx')
+                cIQ.h=winNorm'.*(dataThis.IHx+i*dataThis.QHx)./sqrt(sampleNum);
+            end
+            
+            %% Other variables
+            momentsTimeOne.range(:,ii)=dataThis.range;
+            momentsTimeOne.elevation(ii)=median(dataThis.elevation);
+            momentsTimeOne.eastward_velocity(ii)=median(dataThis.eastward_velocity);
+            momentsTimeOne.northward_velocity(ii)=median(dataThis.northward_velocity);
+            momentsTimeOne.vertical_velocity(ii)=median(dataThis.vertical_velocity);
+            momentsTimeOne.azimuth_vc(ii)=median(dataThis.azimuth_vc);
+            momentsTimeOne.altitude(ii)=median(dataThis.altitude);
+
+            %% Time moments
+            momentsTimeOne=calcMomentsTime(cIQ,ii,momentsTimeOne,dataThis);
+
+            % Censor on V (find missing and NScal)
+            if max(momentsTimeOne.powerV(1:14,ii))<-80 | (momentsTimeOne.powerV(1,ii)>-96 & momentsTimeOne.powerV(1,ii)<-87)
+                censorY=nan(size(momentsTimeOne.range,1),1);
+            else
+                % Censor on SNR and NCP
+                censorY=momentsTimeOne.snr(:,ii)<0 & momentsTimeOne.ncp(:,ii)<0.2;
+                censorY(isnan(momentsTimeOne.snr(:,ii)))=1;
+                censorY=~censorY;
+                censorY=double(censorY);
+                censorY(censorY==0)=nan;
+                censorY=movmedian(censorY,7,'includemissing');
+                censorY=movmedian(censorY,7,'omitmissing');
+            end
+            momentsTimeOne.velRaw(isnan(censorY),ii)=nan;
+
+            %% Correct time domain velocity folding
+
+            if momentsTimeOne.elevation(ii)>0
+                velRay=-momentsTimeOne.velRaw(:,ii);
+            else
+                velRay=momentsTimeOne.velRaw(:,ii);
+            end
+            velRay(1:12)=nan;
+
+            finalRay=deAliasSingleRay(velRay,velPrev,defaultPrev,[],momentsTimeOne.time(ii));
+
+            % Set up time consistency check
+
+            [velPrev,prevCount,prevKeep,flipYes]=setUpPrev(finalRay,velPrev,prevCount,prevKeep,flipYes,momentsTimeOne.elevation(ii),1/outTime,defaultPrev);
+
+            
+            %% Add to output
+            if momentsTimeOne.elevation(ii)>0
+                momentsTimeOne.velRawDeAliased(:,ii)=-finalRay;
+            else
+                momentsTimeOne.velRawDeAliased(:,ii)=finalRay;
+            end
+
+            %% Correct velocity for aircraft motion
+            xCorr=sind(momentsTimeOne.azimuth_vc(ii)).*cosd(momentsTimeOne.elevation(ii)).*momentsTimeOne.eastward_velocity(ii);
+            yCorr=cosd(momentsTimeOne.azimuth_vc(ii)).*cosd(momentsTimeOne.elevation(ii)).*momentsTimeOne.northward_velocity(ii);
+            zCorr=sind(momentsTimeOne.elevation(ii)).*momentsTimeOne.vertical_velocity(ii);
+            momentsTimeOne.vel(:,ii)=momentsTimeOne.velRawDeAliased(:,ii)+single(xCorr+yCorr+zCorr);
+
+            %% Spectral moments
+            [specPowerLin,specPowerDB]=getSpectra(cIQ);
+
+            % Move peak of spectra to middle
+            [specPowerDBadj,specVelAdj]=adjSpecBoundsV(specPowerDB.V,momentsTimeOne.velRawDeAliased(:,ii),dataThis);
+
+            % De-alias
+            % Mean vel
+            noiseLinV=10.^(data.noise_v./10);
+            x=specVelAdj;
+            y=specPowerLin.V-noiseLinV;
+            velMean=sum(y.*x,2,'omitnan')./sum(y,2,'omitnan');
+
+            deAliasDiff=velMean-momentsTimeOne.velRawDeAliased(:,ii);
+
+            deAliasMask=zeros(size(deAliasDiff));
+            checkFold=[2,4,6];
+
+            for jj=1:3
+                deAliasMask(deAliasDiff>checkFold(jj)*defaultPrev-5)=checkFold(jj)*defaultPrev;
+                deAliasMask(deAliasDiff<-(checkFold(jj)*defaultPrev-5))=-checkFold(jj)*defaultPrev;
+            end
+
+            specVelAdj=specVelAdj-deAliasMask;
+          
+            % Censor
+            specPowerDBadj(isnan(momentsTimeOne.vel(:,ii)),:)=nan;
+            specVelAdj(isnan(momentsTimeOne.vel(:,ii)),:)=nan;
+
+            %% Remove noise
+            [powerDBsmooth,powerRMnoiseDBsmooth]=rmNoiseSpec(specPowerDBadj);
+            powerRMnoiseDB=specPowerDBadj;
+            powerRMnoiseDB(isnan(powerRMnoiseDBsmooth))=nan;
+            specVelRMnoise=specVelAdj;
+            specVelRMnoise(isnan(powerRMnoiseDBsmooth))=nan;
+
+            %% Find regions with dual particle species
+
+            [momentsVelDualRawOne,momentsPowerDualRawOne]=findDualVels(powerRMnoiseDBsmooth,specVelRMnoise,specPowerDBadj,momentsVelDualRawOne,momentsPowerDualRawOne,ii);
+
+            %% Multi DBZ
+            % DBM
+            powerLinV=10.^(momentsPowerDualRawOne(:,ii,:)./10);
+            
+            % SNR
+            noiseLinV=10.^(dataThis.noise_v./10);
+            snrLinV=(powerLinV-noiseLinV)./noiseLinV;
+            snrLinV(snrLinV<0)=nan;
+            snrDB=10*log10(snrLinV);
+
+            % DBZ
+            dataThis.range(dataThis.range<0)=nan;
+            momentsPowerDualRawOne(:,ii,:)=snrDB+20*log10(dataThis.range./1000)+data.dbz1km_v;
+        end
+
+        %% Add to output
+        if bb==1
+            momentsTime=momentsTimeOne;
+            momentsVelDualRaw=momentsVelDualRawOne;
+            momentsPowerDualRaw=momentsPowerDualRawOne;
+        else
+            dataFields=fields(momentsTime);
+
+            for hh=1:length(dataFields)
+                momentsTime.(dataFields{hh})=cat(2,momentsTime.(dataFields{hh}),momentsTimeOne.(dataFields{hh}));
+            end
+
+            checkDims=size(momentsVelDualRaw,3)-size(momentsVelDualRawOne,3);
+            if checkDims<0
+                momentsVelDualRaw=padarray(momentsVelDualRaw,[0,0,abs(checkDims)],nan,'post');
+                momentsPowerDualRaw=padarray(momentsPowerDualRaw,[0,0,abs(checkDims)],nan,'post');
+            end
+
+            momVelTest=nan(size(momentsVelDualRawOne,1),size(momentsVelDualRawOne,2),max(size(momentsVelDualRaw,3)));
+            momPowTest=nan(size(momentsVelDualRawOne,1),size(momentsVelDualRawOne,2),max(size(momentsVelDualRaw,3)));
+            dim3=size(momentsVelDualRawOne,3);
+            momVelTest(:,:,1:dim3)=momentsVelDualRawOne;     
+            momentsVelDualRaw=cat(2,momentsVelDualRaw,momVelTest);
+            momPowTest(:,:,1:dim3)=momentsPowerDualRawOne;     
+            momentsPowerDualRaw=cat(2,momentsPowerDualRaw,momPowTest);
+        end
+    end
+    
+    %% Sort out vel dual
+    [momentsVelDual,momentsDbzDual]=sortVelsIntoTwo(momentsVelDualRaw,momentsPowerDualRaw,momentsTime);
+
+    %% Take time
+
+    eSecs=toc;
+
+    eData=momentsTime.time(end)-momentsTime.time(1);
+    timePerMin=eSecs/60/minutes(eData);
+    disp(['Total: ',num2str(eSecs/60),' minutes. Per data minute: ',num2str(timePerMin),' minutes.']);
+
+    %% Plot
+
+    close all
+
+    disp('Plotting velocities ...');
+
+    momentsTime.asl=HCRrange2asl(momentsTime.range,momentsTime.elevation,momentsTime.altitude);
+
+    plotVelocities(momentsVelDual,momentsTime,figdir,project,showPlot);
+    plotReflectivities(momentsDbzDual,momentsTime,figdir,project,showPlot);
+
+end
---
+++ projDir/qc/dataProcessing/spectralHCRproducts/multiVels/dualVel/findDualVels.m
@@ -0,0 +1,164 @@
+function [momentsVelDual,momentsPowDual]=findDualVels(powerIn,specVelIn,powerRaw,momentsVelDual,momentsPowDual,nn)
+% Find maxima and minima in spectra
+velDual=nan(size(powerIn,1),35);
+powDual=nan(size(powerIn,1),35);
+
+dataInds=find(any(~isnan(powerIn),2));
+
+% Remove transmitter pulse
+dataInds(dataInds<=12)=[];
+
+locsMaxAll=islocalmax(powerIn,2);
+
+diffCurve=cat(2,nan(size(powerIn,1),1),diff(powerIn,1,2));
+diffMaxAll=islocalmax(diffCurve,2);
+diffMinAll=islocalmin(diffCurve,2);
+
+for jj=1:length(dataInds)
+    ii=dataInds(jj);
+    
+    powerOrig=powerIn(ii,:);
+
+    % Find start and end of line segments
+    lineMask=~isnan(powerOrig);
+    diffLine=diff(lineMask);
+
+    startLine=find(diffLine==1)+1;
+    endLine=find(diffLine==-1);
+
+    if isempty(startLine) & isempty(endLine)
+        startLine=1;
+        endLine=length(powerOrig);
+    end
+
+    if isempty(startLine) & ~isempty(endLine)
+        startLine=1;
+    end
+    if ~isempty(startLine) & isempty(endLine)
+        endLine=length(powerOrig);
+    end
+    if startLine(1)>endLine(1)
+        startLine=[1,startLine];
+    end
+    if startLine(end)>endLine(end)
+        endLine=[endLine,length(powerOrig)];
+    end
+
+    % Find maxima % and minima
+    locsMax=find(locsMaxAll(ii,:)==1);
+   
+    % Find change points
+    diffMax=find(diffMaxAll(ii,:)==1);
+    diffMin=find(diffMinAll(ii,:)==1);
+
+    % Find points to test
+    testX=[];
+    testY=[];
+
+    for kk=1:length(diffMax)-1
+        if powerOrig(diffMax(kk+1))>powerOrig(diffMax(kk))
+            findMin=sum(diffMin>diffMax(kk) & diffMin<diffMax(kk+1));
+            if findMin==1
+                hasPeak=sum(locsMax>diffMax(kk) & locsMax<diffMax(kk+1));
+                if hasPeak==0
+                    testX=cat(1,testX,[diffMax(kk),diffMax(kk+1)]);
+                end
+            end
+        end
+    end
+    for kk=1:length(diffMin)-1
+        if powerOrig(diffMin(kk))>powerOrig(diffMin(kk+1))
+            findMax=sum(diffMax>diffMin(kk) & diffMax<diffMin(kk+1));
+            if findMax==1
+                hasPeak=sum(locsMax>diffMin(kk) & locsMax<diffMin(kk+1));
+                if hasPeak==0
+                    testX=cat(1,testX,[diffMin(kk),diffMin(kk+1)]);
+                end
+            end
+        end
+    end
+        
+    addPeak=[];
+    for kk=1:size(testX,1)
+        smallPeakInds=[];
+        testXthis=testX(kk,:);
+        testYthis=powerOrig(testXthis);
+        testY=cat(1,testY,testYthis);
+        fitLineMod=polyfit(testXthis,testYthis,1);
+        fitLine=polyval(fitLineMod,testX(kk,1):testX(kk,2));
+        powerMinusLine=powerOrig(testXthis(1):testXthis(2))-fitLine;
+        cutProm=3;
+        testProm=max(powerMinusLine)-min(powerMinusLine);
+        if testProm>cutProm
+            [~,smallPeaks]=max(powerMinusLine);
+            smallPeakInds=smallPeaks+testXthis(1);
+        end
+        if powerOrig(testXthis(1))<powerOrig(testXthis(2))
+            for ll=1:length(smallPeakInds)
+                goodPeak=smallPeakInds(ll);
+                peakValDiff=goodPeak-diffMin;
+                getPeak=diffMin(min(find(peakValDiff<0)));
+                if getPeak<testXthis(2)
+                    addPeak=cat(2,addPeak,getPeak);
+                end
+            end
+        else
+            for ll=1:length(smallPeakInds)
+                goodPeak=smallPeakInds(ll);
+                peakValDiff=goodPeak-diffMax;
+                getPeak=diffMax(max(find(peakValDiff>0)));
+                if getPeak>testXthis(1)
+                    addPeak=cat(2,addPeak,getPeak);
+                end
+            end
+        end
+    end
+
+        locsMax=cat(2,locsMax,addPeak);
+
+        %% Add to output
+
+    % Check against noise floor
+    if sum(~isnan(locsMax))>2
+        minPow=min(powerOrig,[],'omitmissing');
+        testPow=powerOrig(locsMax);
+        absDiff=abs(testPow-minPow);
+        locsMax(absDiff<5)=[];
+        if isempty(locsMax)
+            [~,locsMax]=max(powerOrig,[],'omitmissing');
+        end
+    end
+
+    velDual(ii,1:length(locsMax))=specVelIn(ii,locsMax);
+    powDual(ii,1:length(locsMax))=powerIn(ii,locsMax);
+
+    if nn==inf
+        scatter(specVelIn(ii,:),powerRaw(ii,:),'MarkerEdgeColor','k');
+        xlim([-8,8]);
+        hold on
+        plot(specVelIn(ii,:),powerOrig,'-b','linewidth',2);
+        scatter(specVelIn(ii,locsMax(~isnan(locsMax))),powerOrig(locsMax(~isnan(locsMax))),80,'filled','MarkerFaceColor','r')
+        scatter(specVelIn(ii,diffMin),powerOrig(diffMin),'filled','MarkerFaceColor','y')
+        scatter(specVelIn(ii,diffMax),powerOrig(diffMax),'filled','MarkerFaceColor','k')
+        if ~isempty(testX)
+            for mm=1:size(testX,1)
+                scatter(specVelIn(ii,testX(mm,:)),testY(mm,:),'filled','MarkerFaceColor','g')
+            end
+        end
+        cla
+    end
+end
+findEmpty=all(isnan(velDual),1);
+velDual(:,findEmpty)=[];
+powDual(:,findEmpty)=[];
+
+checkDims=size(momentsVelDual,3)-size(velDual,2);
+if checkDims<0
+    momentsVelDual=padarray(momentsVelDual,[0,0,abs(checkDims)],nan,'post');
+    momentsPowDual=padarray(momentsPowDual,[0,0,abs(checkDims)],nan,'post');
+end
+
+dim3=size(velDual,2);
+momentsVelDual(:,nn,1:dim3)=single(velDual);
+momentsPowDual(:,nn,1:dim3)=single(powDual);
+end
