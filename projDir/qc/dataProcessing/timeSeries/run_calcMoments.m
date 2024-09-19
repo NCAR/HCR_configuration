d770a8fafd93b292fd5609e8e90a3a4af4a8bdf9
mike-dixon <dixon@ucar.edu>
2024-09-19 17:23:40 -0600

    Moving function

--- projDir/qc/dataProcessing/timeSeries/run_calcMoments.m
+++ projDir/qc/dataProcessing/timeSeries/run_calcMoments.m
@@ -4,11 +4,11 @@ close all;
 
 addpath(genpath('~/git/HCR_configuration/projDir/qc/dataProcessing/'));
 
-project='spicule'; %socrates, aristo, cset, otrec
+project='noreaster'; %socrates, aristo, cset, otrec
 quality='ts'; %field, qc1, or qc2
-qualityCF='qc1';
+qualityCF='qc2';
 freqData='10hz';
-qcVersion='v1.2';
+qcVersion='v2.0';
 
 %plotInds=0;
 plotInds=(1:50:500);
