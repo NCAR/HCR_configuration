2e9dfa142cd3ee5243c46833b5063699a9919df9
mike-dixon <dixon@ucar.edu>
2024-10-30 14:42:10 -0600

    More MEOW QC

--- projDir/qc/dataProcessing/utils/modelDir.m
+++ projDir/qc/dataProcessing/utils/modelDir.m
@@ -1,13 +1,13 @@
 function [rawDir interp] = modelDir(project,model,qc,qcVersion,freq)
 % Find model directory
 if strcmp(project,'socrates')
-    baseDir='/scr/snow2/rsfdata/projects/socrates/';
+    baseDir='/scr/virga2/rsfdata/projects/socrates/';
 elseif strcmp(project,'cset')
     baseDir='/scr/snow2/rsfdata/projects/cset/';
 elseif strcmp(project,'otrec')
     baseDir='/scr/sleet2/rsfdata/projects/otrec/';
 elseif strcmp(project,'spicule')
-    baseDir='/scr/sleet3/rsfdata/projects/spicule/';
+    baseDir='/scr/virga1/rsfdata/projects/spicule/';
 elseif strcmp(project,'noreaster')
     baseDir='/scr/snow2/rsfdata/projects/noreaster/';
 end
